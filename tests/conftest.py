9365b96eaff753f5eb420147f336c523ff9702c8
Vyacheslav Matyukhin <me@berekuk.ru>
2018-01-21 02:58:46 +0300

    get rid of pytest-flask

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
 import pytest
 import os
+import json
+from flask import Response
 
 os.environ['TIER'] = 'dev'
 os.environ['JWT_SECRET_KEY'] = 'testkey'
 
 import kocherga.api.app
 
-@pytest.fixture
-def app():
-    return kocherga.api.app.create_app(DEV=True)
+class TestResponse(Response):
+    @property
+    def json(self):
+        return json.loads(self.data)
+
+@pytest.fixture('session')
+def api_client():
+    app = kocherga.api.app.create_app(DEV=True)
+    app.response_class = TestResponse
+    # Don't set `app.testing = True` - we want to distinguish PublicError from generic Exceptions to check for information leaks. Eventually.
+    return app.test_client()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import json
 
-def test_events(client):
-    res = client.get('/events')
+def test_events(api_client):
+    res = api_client.get('/events')
     assert res.status_code == 200
     events = res.json
 
@@ -10,8 +10,8 @@ def test_events(client):
 
     assert '2017' in events[0]['start']['dateTime']
 
-def test_events_from_date(client):
-    res = client.get('/events?from_date=2018-01-01&to_date=2018-01-14')
+def test_events_from_date(api_client):
+    res = api_client.get('/events?from_date=2018-01-01&to_date=2018-01-14')
     assert res.status_code == 200
     events = res.json
 
@@ -19,12 +19,12 @@ def test_events_from_date(client):
 
     assert '2018' in events[0]['start']['dateTime']
 
-def test_bookings(client):
-    res = client.get('/bookings/today')
+def test_bookings(api_client):
+    res = api_client.get('/bookings/today')
     assert res.status_code == 200
 
-def test_rooms(client):
-    res = client.get('/rooms')
+def test_rooms(api_client):
+    res = api_client.get('/rooms')
     assert res.status_code == 200
     assert len(res.json) == 4
     assert type(res.json[0]) == dict
