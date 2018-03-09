6c95c3470f984ff231baa12b3b9629d95e0c8fcd
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-10 02:57:16 +0300

    templater/png/<name> works!

--- kocherga/api/routes/templater.py
+++ kocherga/api/routes/templater.py
@@ -1,20 +1,78 @@
-from quart import Blueprint, request, render_template
 import datetime
+import re
+import asyncio
+
+from quart import Blueprint, request, render_template, make_response
+
+import pyppeteer
 
 from kocherga.api.auth import auth
 
 bp = Blueprint('templater', __name__)
 
+_browser = None
+async def get_browser():
+    global _browser
+    if not _browser:
+        _browser = await pyppeteer.launch()
+    return _browser
+
 @bp.app_template_filter()
 def parse_date(value, fmt):
     return datetime.datetime.strptime(value, fmt)
 
-@bp.route('/templater/html/<name>')
-#@auth('kocherga')
-async def generate_html(name):
-    args = {}
+def get_args(args, form, host):
+    result = {}
 
-    for k, v in request.args.items():
-        args[k] = v
-    args.update(await request.form)
+    for k, v in args.items():
+        result[k] = v
+    result.update(form)
+
+    # Sorry, this is a hack - I can't find a way to extract the protocol from quart directly, so I have to use this heuristic:
+    # everything localhost is http, everything else is https.
+    schema = 'http' if host.startswith('localhost') else 'https'
+    result['url_root'] = f'{schema}://{host}'
+
+    return result
+
+async def get_html(args, name):
     return await render_template(f'templater/{name}.html', **args)
+
+@bp.route('/templater/html/<name>')
+@auth('kocherga')
+async def generate_html(name):
+    return await get_html(
+        get_args(request.args, await request.form, request.headers['Host']),
+        name
+    )
+
+@bp.route('/templater/png/<name>')
+@auth('kocherga')
+async def generate_png(name):
+    html = await get_html(
+        get_args(request.args, await request.form, request.headers['Host']),
+        name
+    )
+
+    (width, height) = (800, 600) # should we fast fail instead?
+    match = re.search(r'<!-- width=(\d+) height=(\d+)', html)
+    if match:
+        (width, height) = (int(v) for v in match.groups())
+
+    browser = await get_browser()
+    page = await browser.newPage()
+    await page.setViewport({
+        'width': width,
+        'height': height,
+    })
+    await page.goto(f'data:text/html,{html}', {
+        'waitUntil': 'load',
+        'timeout': 10000,
+    })
+
+    image_bytes = await page.screenshot()
+    await page.close()
+
+    response = await make_response(image_bytes)
+    response.headers['Content-Type'] = 'image/png'
+    return response
