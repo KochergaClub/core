93becbbd4d4bed2377d6002a0af4d5cbddd8cfc8
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-10 01:10:07 +0300

    remove an old update_credentials.py

--- update_credentials.py
+++
@@ -1,7 +0,0 @@
-import kocherga.calendar_api
-from oauth2client import tools
-
-import argparse
-
-flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-kocherga.calendar_api.update_credentials(flags)
