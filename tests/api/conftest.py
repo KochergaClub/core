e8bb32141b9b1eb51e93d3e3431b86ced5202909
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-13 04:21:52 +0300

    tests.api: TestResponse is not needed since we moved from flask to quart

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -8,14 +8,7 @@ os.environ['JWT_SECRET_KEY'] = 'testkey'
 
 import kocherga.api.app
 
-#class TestResponse(Response):
-#    @property
-#    def json(self):
-#        return json.loads(self.data)
-
 @pytest.fixture('session')
 def api_client():
     app = kocherga.api.app.create_app(DEV=True)
-    # app.response_class = TestResponse
-    # Don't set `app.testing = True` - we want to distinguish PublicError from generic Exceptions to check for information leaks. Eventually.
     return app.test_client()
