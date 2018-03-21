e6adc236c5931481b80c5083bda0a1fa465ec641
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-21 04:10:39 +0300

    improve importer code and log tables

--- kocherga/importer/base.py
+++ kocherga/importer/base.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String
 from abc import ABC, abstractmethod
 
-from typing import Any
+from typing import Any, Optional
 
 import kocherga.db
 from kocherga.config import TZ
@@ -12,26 +12,64 @@ from kocherga.config import TZ
 class ImporterState(kocherga.db.Base):
     __tablename__ = 'importers_state'
     name = Column(String, primary_key=True)
-    timestamp = Column(Integer)
+    until_ts = Column(Integer)
+    last_ts = Column(Integer)
+    last_exception = Column(String)
 
     @classmethod
     def init_db(cls):
         cls.__table__.create(bind=kocherga.db.engine())
 
     @property
-    def dt(self) -> datetime:
-        return datetime.fromtimestamp(self.timestamp, TZ)
+    def until_dt(self) -> Optional[datetime]:
+        if not self.until_ts:
+            return None
+        return datetime.fromtimestamp(self.until_ts, TZ)
 
 class ImporterLogEntry(kocherga.db.Base):
     __tablename__ = 'importers_log'
     id = Column(Integer, primary_key=True)
     name = Column(String, index=True)
-    timestamp = Column(Integer)
+    start_ts = Column(Integer)
+    end_ts = Column(Integer)
+    exception = Column(String)
 
     def __init__(self, name):
         self.name = name
-        self.timestamp = datetime.now(TZ).timestamp()
+        self.start_ts = datetime.now(TZ).timestamp()
 
+class ImportContext:
+    def __init__(self, name, mode):
+        self.name = name
+        self.mode = mode
+        self.session = kocherga.db.Session()
+
+    def __enter__(self):
+        self.log_entry = ImporterLogEntry(self.name)
+        self.state = self.session.query(ImporterState).filter_by(name=self.name).first()
+        if not self.state:
+            self.state = ImporterState(name=self.name)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print('__exit__')
+        print(exc_type, exc_value, traceback)
+        self.state.last_ts = datetime.now(TZ).timestamp()
+
+        if exc_value:
+            # let's drop everything from our failed session
+            self.session = kocherga.db.Session()
+
+        self.state.last_exception = str(exc_value)
+
+        self.log_entry.end_ts = self.state.last_ts
+        self.log_entry.exception = self.state.last_exception
+
+        self.session.merge(self.state)
+        self.session.add(self.log_entry)
+
+        self.session.commit()
+        logging.info(f'{self.name} imported')
 
 class BaseImporter(ABC):
     @abstractmethod
@@ -58,11 +96,8 @@ class FullImporter(BaseImporter):
         pass
 
     def import_new(self) -> None:
-        session = kocherga.db.Session()
-        self.do_full_import(session)
-        session.add(ImporterLogEntry(self.name))
-        session.commit()
-        logging.info(f'{self.name} imported')
+        with ImportContext(self.name, 'full') as ic:
+            self.do_full_import(ic.session)
 
 
 class IncrementalImporter(BaseImporter):
@@ -75,31 +110,23 @@ class IncrementalImporter(BaseImporter):
     def do_period_import(self, from_dt: datetime, to_dt: datetime, session: object) -> datetime:
         pass
 
-    def _state(self, session: Any) -> ImporterState:
-        state = session.query(ImporterState).filter_by(name=self.name).first()
-        if state:
-            return state
-        return ImporterState(name=self.name, timestamp=self.get_initial_dt().timestamp())
+    def _import(self, mode: str) -> None:
+        with ImportContext(self.name, mode) as ic:
+            if mode == 'all' or not ic.state.until_dt:
+                start_dt = self.get_initial_dt()
+            else:
+                start_dt = ic.state.until_dt
+
+            end_dt = datetime.now(TZ)
+
+            last_dt = self.do_period_import(start_dt, end_dt, ic.session)
+            print(last_dt)
+            if not last_dt:
+                raise Exception(f"{self.name}.do_period_import didn't return a datetime object")
+            ic.state.until_ts = last_dt.timestamp()
 
     def import_all(self) -> None:
-        session = kocherga.db.Session()
-
-        last_dt = self.do_period_import(self.get_initial_dt(), datetime.now(TZ), session)
-        state = self._state(session)
-        state.timestamp = last_dt.timestamp()
-        session.merge(state)
-        session.add(ImporterLogEntry(self.name))
-        session.commit()
-        logging.info(f'{state.name} -> {state.dt}')
+        self._import('all')
 
     def import_new(self) -> None:
-        session = kocherga.db.Session()
-        state = self._state(session)
-
-        last_dt = self.do_period_import(state.dt, datetime.now(TZ), session)
-
-        state.timestamp = last_dt.timestamp()
-        session.merge(state)
-        session.add(ImporterLogEntry(self.name))
-        session.commit()
-        logging.info(f'{state.name} -> {state.dt}')
+        self._import('new')
--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,9 +1,10 @@
-import kocherga.importer.base
+from kocherga.importer.base import IncrementalImporter, ImporterState, ImporterLogEntry
 import kocherga.db
 
 from datetime import datetime, timedelta
+from kocherga.config import TZ
 
-class SomeImporter(kocherga.importer.base.IncrementalImporter):
+class SomeImporter(IncrementalImporter):
     def get_initial_dt(self):
         return datetime.now() - timedelta(days=10)
 
@@ -14,7 +15,18 @@ class SomeImporter(kocherga.importer.base.IncrementalImporter):
     def init_db(self):
         pass
 
-def test_importer(db):
+def test_import_new(db):
+    importer = SomeImporter()
+    importer.import_new()
+
+    state = kocherga.db.Session().query(ImporterState).filter(ImporterState.name == 'test_importer.SomeImporter').first()
+    assert state.last_exception == None
+    assert abs(datetime.now(tz=TZ).timestamp() - state.last_ts) < 5
+
+def test_import_all(db):
     importer = SomeImporter()
     importer.import_all()
-    importer.import_new()
+
+    state = kocherga.db.Session().query(ImporterState).filter(ImporterState.name == 'test_importer.SomeImporter').first()
+    assert state.last_exception == None
+    assert abs(datetime.now(tz=TZ).timestamp() - state.last_ts) < 5
