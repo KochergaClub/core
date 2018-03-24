1febf6f92902d0ea1d60b6a86eae061eb14990c7
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-24 05:23:31 +0300

    ludwig crypto check: fix some false-negatives

--- kocherga/ludwig/crypto.py
+++ kocherga/ludwig/crypto.py
@@ -1,8 +1,10 @@
 import requests
+from datetime import datetime
 
 from kocherga.ludwig.bot import bot
 
 ETH_ADDRESS = '0x97b130467259ae2a7e67ecb9d39e2b366174e182'
+MAX_SHARE_DELAY = 3600
 
 @bot.schedule('cron', hour='11,23')
 def check_miners():
@@ -22,11 +24,16 @@ def check_miners():
             'name': 'Рига (большая коробка)',
         },
     ]
+    current_ts = datetime.now().timestamp()
+
     for device in devices:
-        if device['id'] in workers_dict and workers_dict[device['id']]['hashrate'] > 0:
+        if device['id'] in workers_dict and (
+                workers_dict[device['id']]['hashrate'] > 0
+                or current_ts - workers_dict[device['id']]['lastShare'] < MAX_SHARE_DELAY
+        ):
             # ok
             continue
         bot.send_message(
-            text='Админ, майнинг на устройстве "{}" не работает. Почини, если нет к этому препятствий.'.format(device['name']),
+            text=f'''Админ, майнинг на устройстве "{device['name']}" не работает. Почини, если нет к этому препятствий.\nПодробнее: <https://wiki.admin.kocherga.club/Майнинг>''',
             channel='#watchmen',
         )
