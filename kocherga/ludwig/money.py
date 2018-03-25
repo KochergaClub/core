698d1edab4c2982538ef9d66ea4c11ec0fe56277
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-25 19:48:04 +0300

    ludwig cash: understand verbal request

--- kocherga/ludwig/money.py
+++ kocherga/ludwig/money.py
@@ -2,7 +2,14 @@ from kocherga.ludwig.bot import bot
 
 import kocherga.money.cashier
 
-@bot.command('/cash')
-def command_current_cash(payload):
+def cash_response():
     cash = kocherga.money.cashier.current_cash()
     return f'В кассе {cash} руб.'
+
+@bot.listen_to(r'сколько\s+(сейчас\s+)?денег\s+в\s+кассе')
+def react_current_cash(message):
+    message.reply(cash_response())
+
+@bot.command('/cash')
+def command_current_cash(payload):
+    return cash_response()
