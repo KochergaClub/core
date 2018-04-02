570bf902ca30aa64b2b6a21e5003bfc1d745363e
Vyacheslav Matyukhin <me@berekuk.ru>
2018-04-03 02:56:29 +0300

    backup scripts sys path

--- scripts/backup-mysql.py
+++ scripts/backup-mysql.py
@@ -1,5 +1,6 @@
-
 #!/usr/bin/env python
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 import boto3
 import logging
--- scripts/backup-sqlite.py
+++ scripts/backup-sqlite.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 import boto3
 import logging
@@ -11,10 +13,12 @@ AWS_CONFIG = kocherga.secrets.json_secret('aws_credentials')
 BUCKET_NAME = kocherga.config.config()['s3']['backups']
 
 def main():
-    logging.info('Backing up database...')
+    logging.info('Backing up SQLite database...')
 
     s3 = boto3.session.Session(**AWS_CONFIG).client('s3')
     s3.upload_file(kocherga.db.DB_FILE, BUCKET_NAME, 'kocherga.db')
-    logging.info('Backup successful')
+    logging.info('SQLite db backup successful')
+
+    shell.subprocess('mysqldump --databases {{kocherga.wiki.db.name}} {{kocherga.metabase.db.name}} {{kocherga.passbolt.db.name}')
 
 main()
