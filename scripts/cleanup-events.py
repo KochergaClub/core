7d2933879f997dbd088cd33226ff07062e804b66
Vyacheslav Matyukhin <me@berekuk.ru>
2018-04-03 00:30:46 +0300

    cleanup-events.py is done (and not needed anymore)

--- scripts/cleanup-events.py
+++ scripts/cleanup-events.py
@@ -1,43 +1,61 @@
 #!/usr/bin/env python
+import json
+import os
 
 from datetime import datetime, timedelta
 from kocherga.config import TZ
 
 import kocherga.events.google
 
+def get_pp(ge):
+    return ge.get('extendedProperties', {}).get('private', {})
+
+def get_prop(ge, prop):
+    return get_pp(ge).get(prop, None)
+
 def grep_prop(prop, events):
     for ge in events:
-            pp = ge.get('extendedProperties', {}).get('private', {})
-            if prop in pp:
-                    yield ge
+        if prop in get_pp(ge):
+            yield ge
 
-all_pp = [
-    'bar',
-    'foo',
-    'posted-timepad',
-    'posted-fb',
-    'asked_for_visitors',
-    'visitors',
-    'has_default_image',
-    'type',
-    'has_vk_image',
-    'timepad',
-    '_timepad_',
-    'hello',
-    'has_image',
-    'ready-to-post',
-    'fb_group',
-    'vk-link',
-    'vk_group',
-    'posted-vk'
-]
+def all_pp(events):
+    result = set()
+    for ge in events:
+        result = result | set(ge.get('extendedProperties', {}).get('private', {}))
+    return list(result)
 
-events = kocherga.events.google.list_events(to_date=(datetime.now(tz=TZ) + timedelta(days=7*8)).date())
+def stat(events):
+    for pp in sorted(all_pp(events)):
+        pp_events = list(grep_prop(pp, events))
+        pp_values = list(
+            (ge['id'], ge['extendedProperties']['private'][pp])
+            for ge in pp_events
+        )
+        unique_pp_values = set(list(v[0] for v in pp_values))
+        print(f'{pp} -> {len(pp_events)}, unique values = {len(unique_pp_values)}, values = {pp_values[:1]}')
 
-for pp in all_pp:
-    pp_events = list(grep_prop(pp, events))
-    pp_values = list(set(
-        ge['extendedProperties']['private'][pp]
-        for ge in pp_events
-    ))
-    print(f'{pp} -> {len(pp_events)}, unique values = {len(pp_values)}, values = {pp_values[:5]}')
+def main():
+    if os.environ.get('RELOAD'):
+        events = kocherga.events.google.list_events(to_date=(datetime.now(tz=TZ) + timedelta(days=7*8)).date())
+        with open('events.json', 'w') as fh:
+            fh.write(json.dumps(events))
+    else:
+        with open('events.json') as fh:
+            events = json.load(fh)
+
+    fe = list(grep_prop('vk-link', events))
+    print(len(fe))
+    #for ge in fe:
+    #    old = get_prop(ge, 'vk-link')
+    #    new = get_prop(ge, 'vk_group')
+    #    if new is not None and new != old:
+    #        raise NotImplemented
+
+    #    print(f"{ge['id']:<80}\t{old:<20}\t{new}")
+    #    kocherga.events.google.set_property(ge['id'], 'vk-link', None)
+
+    stat(events)
+
+main()
+
+# All migrations are complete. This script stays here just for a historical interest and can be removed later at any time.
