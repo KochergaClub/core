3f81cea223373c065d3e7262a299ed10ffc03ea8
Vyacheslav Matyukhin <me@berekuk.ru>
2018-04-26 02:40:13 +0300

    move http api static assets to the correct location

--- scripts/elba-cash-income.py
+++ scripts/elba-cash-income.py
@@ -12,11 +12,11 @@ import kocherga.money.elba
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    filename = '/Users/berekuk/Downloads/query_result_2018-03-28T01_17_47.234Z.json'
+    filename = '/Users/berekuk/Downloads/query_result_2018-04-25T11_28_59.559Z.json'
     await kocherga.money.elba.add_cash_income(
         json.load(open(filename)),
-        732,
-        from_date=datetime(2018,1,15).date()
+        802,
+        from_date=datetime(2018,3,27).date()
     )
 
 loop = asyncio.get_event_loop()
