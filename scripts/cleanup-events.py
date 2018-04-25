20b8ac16eb53804162bdcf95386fd26c242b0d3b
Vyacheslav Matyukhin <me@berekuk.ru>
2018-04-25 13:22:33 +0300

    fix path in all scripts

--- scripts/cleanup-events.py
+++ scripts/cleanup-events.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+
 import json
 import os
 
--- scripts/elba-ion.py
+++ scripts/elba-ion.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
 
 import logging
 import asyncio
