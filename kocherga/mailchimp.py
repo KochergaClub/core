80ffb968d458f879afe3321e4d492743bce4f1fd
Vyacheslav Matyukhin <me@berekuk.ru>
2018-05-24 21:28:50 +0300

    refactor sheet2mailchimp to kocherga.ratio.users and kocherga.mailchimp

---
+++ kocherga/mailchimp.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+import kocherga.secrets
+
+MAILCHIMP_API = 'https://us11.api.mailchimp.com/3.0'
+MAILCHIMP_API_KEY = kocherga.secrets.plain_secret('mailchimp_api_key')
+
+def api_call(method, url, data={}):
+    if method == 'GET':
+        r = requests.get(
+            f'{MAILCHIMP_API}/{url}',
+            headers={
+                'Authorization': f'apikey {MAILCHIMP_API_KEY}',
+            }
+        )
+    elif method == 'POST':
+        r = requests.post(
+            f'{MAILCHIMP_API}/{url}',
+            headers={
+                'Authorization': f'apikey {MAILCHIMP_API_KEY}',
+                'Content-Type': 'application/json',
+            },
+            data=json.dumps(data),
+        )
+    elif method == 'PUT':
+        # copy-pasted!
+        r = requests.put(
+            f'{MAILCHIMP_API}/{url}',
+            headers={
+                'Authorization': f'apikey {MAILCHIMP_API_KEY}',
+                'Content-Type': 'application/json',
+            },
+            data=json.dumps(data),
+        )
+    else:
+        raise Exception(f'Unknown method {method}')
+
+    r.raise_for_status()
+    return r.json()
