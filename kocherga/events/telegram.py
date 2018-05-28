3a95879d100bfa0f64532922649a921b15f0ea39
Vyacheslav Matyukhin <me@berekuk.ru>
2018-05-28 18:05:04 +0300

    kocherga.telegram and kocherga.events.telegram

---
+++ kocherga/events/telegram.py
@@ -0,0 +1,50 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from datetime import datetime, timedelta
+
+import kocherga.datetime
+
+from kocherga.telegram import post_to_channel
+
+from kocherga.db import Session
+from kocherga.events.event import Event
+
+def post_schedule():
+    message = "#расписание_кочерги\nНа этой неделе в Кочерге:"
+
+    message += "\n\n"
+
+    dt = datetime.today()
+    if dt.weekday() < 2:
+        dt = dt - timedelta(days=dt.weekday())
+    else:
+        dt = dt + timedelta(days=7 - dt.weekday())
+
+    query = (
+        Session()
+        .query(Event)
+        .filter(Event.start_ts > dt.timestamp())
+        .filter(Event.start_ts < (dt + timedelta(weeks=1)).timestamp())
+        .filter(Event.posted_vk != None)
+        .filter(Event.posted_vk != "")
+    )
+
+    events = query.order_by(Event.start_ts).all()
+    logger.info(f"Schedule includes {len(events)} events")
+
+    prev_date = None
+    for event in events:
+        if event.start_dt.date() != prev_date:
+            weekday = kocherga.datetime.weekday(event.start_dt).upper()
+            month = kocherga.datetime.inflected_month(event.start_dt)
+            message += f"{weekday}, {event.start_dt.day} {month}\n"
+            prev_date = event.start_dt.date()
+
+        title = event.title
+        if event.posted_vk:
+            title = f"[{title}]({event.posted_vk})"
+        message += f"{event.start_dt:%H:%M} {title}\n"
+        message += f"{event.generate_summary()}\n\n"
+
+    post_to_channel(message)
---
+++ kocherga/telegram.py
@@ -0,0 +1,22 @@
+import requests
+
+import kocherga.config
+import kocherga.secrets
+
+def get_token():
+    return kocherga.secrets.plain_secret('telegram_token')
+
+def get_channel_id():
+    return kocherga.config.config()['telegram']['channel']
+
+def post_to_channel(message):
+    token = get_token()
+    requests.get(
+        f'https://api.telegram.org/bot{token}/sendMessage',
+        params={
+            'chat_id': get_channel_id(),
+            'text': message,
+            'parse_mode': 'markdown',
+            'disable_web_page_preview': 'true',
+        }
+    )
