7ab6b8f4883c4ee858b85c4b63ea0c58e0a5a186
Vyacheslav Matyukhin <me@berekuk.ru>
2018-06-03 17:28:01 +0300

    black ludwig.bot

--- kocherga/ludwig/bot.py
+++ kocherga/ludwig/bot.py
@@ -13,24 +13,32 @@ SLACK_WORKPLACE_TOKEN = kocherga.slack.token()
 
 PORT = kocherga.config.config()["ludwig_port"]
 
+
 class Bot(slappy.bot):
+
     def cleanup_on_exception(self):
         kocherga.db.Session.remove()
 
+
 def create_bot():
     bot = slappy.Bot(
-        PORT, SLACK_WORKPLACE_TOKEN, SLACK_VERIFICATION_TOKEN,
-        timezone=pytz.timezone('Europe/Moscow') # can't use kocherga.config.TZ - it's based on dateutil.tz now
+        PORT,
+        SLACK_WORKPLACE_TOKEN,
+        SLACK_VERIFICATION_TOKEN,
+        timezone=pytz.timezone(
+            "Europe/Moscow"
+        ),  # can't use kocherga.config.TZ - it's based on dateutil.tz now
     )
 
-    bot.flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    bot.flask_app.config['SQLALCHEMY_DATABASE_URI'] = kocherga.db.DB_URL
+    bot.flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    bot.flask_app.config["SQLALCHEMY_DATABASE_URI"] = kocherga.db.DB_URL
     kocherga.db.Session.replace(SQLAlchemy(bot.flask_app).session)
 
-    sentry_dsn = kocherga.config.config().get('sentry', {}).get('ludwig', None)
+    sentry_dsn = kocherga.config.config().get("sentry", {}).get("ludwig", None)
     if sentry_dsn:
         sentry = Sentry(bot.flask_app, dsn=sentry_dsn, wrap_wsgi=False)
 
     return bot
+
 
 bot = create_bot()
