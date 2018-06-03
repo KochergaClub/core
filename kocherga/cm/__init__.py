cbc30cc302a62a879ef6fb5c138f41d917d55783
Vyacheslav Matyukhin <me@berekuk.ru>
2018-06-03 21:45:43 +0300

    "Макс" in deleted cm users

--- kocherga/cm/__init__.py
+++ kocherga/cm/__init__.py
@@ -338,7 +338,7 @@ def load_users():
     users = []
 
     # needed for OrderLogEntries
-    for i, name in enumerate(["Мальбург", "Андрей Ершов"]):
+    for i, name in enumerate(["Мальбург", "Андрей Ершов", "Макс"]):
         deleted_user = User(id=-i, name=name, login=name, level="deleted")
         users.append(deleted_user)
 
