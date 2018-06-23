0a9f3dc806de405391d42b3efc7efcb9d24ef1ae
Vyacheslav Matyukhin <me@berekuk.ru>
2018-06-23 21:45:01 +0300

    fix prototype tests

--- tests/test_events_prototype.py
+++ tests/test_events_prototype.py
@@ -1,13 +1,16 @@
+import pytest
+pytestmark = pytest.mark.usefixtures('db')
+
 from kocherga.events.prototype import EventPrototype
 from kocherga.db import Session
 
 def test_suggestions():
-    ep = EventPrototype(weekday=3, hour=20, minute=30)
+    ep = EventPrototype(title='foo', weekday=3, hour=20, minute=30, length=30)
     print(ep.suggested_dates())
     assert type(ep.suggested_dates()) == list
 
 def test_new():
-    ep = EventPrototype(weekday=3, hour=20, minute=30)
+    ep = EventPrototype(title='foo', weekday=3, hour=20, minute=30, length=30)
     dt = ep.suggested_dates()[0]
     event = ep.new_event(dt)
     Session().commit()
