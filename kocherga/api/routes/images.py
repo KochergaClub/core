c0638087a130e04e5e0eebc59ae3cd42edb4ebc0
Vyacheslav Matyukhin <me@berekuk.ru>
2018-07-18 22:32:08 +0300

    fix /images/ route

--- kocherga/api/routes/images.py
+++ kocherga/api/routes/images.py
@@ -3,6 +3,8 @@ logger = logging.getLogger(__name__)
 
 from quart import Blueprint, send_file
 
+from kocherga.images import image_storage
+
 bp = Blueprint("images", __name__)
 
 @bp.route("/images/<image_id>")
