39fc2e8a67c9080f00128cd5d04579476a50de26
Vyacheslav Matyukhin <me@berekuk.ru>
2018-07-30 01:04:42 +0300

    fix now api test

--- tests/api/api_people_test.py
+++ tests/api/api_people_test.py
@@ -5,5 +5,4 @@ async def test_now(api_client):
     res = await api_client.get('/people/now')
     assert res.status_code == 200
     now = await res.get_json()
-    assert type(now['now']), int
     assert type(now['total']), int
