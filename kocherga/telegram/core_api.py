78f997b8ed2bed9e76be1fdbaeb94426e49bca57
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-12 18:11:42 +0300

    telegram - fix import client

--- kocherga/telegram/core_api.py
+++ kocherga/telegram/core_api.py
@@ -1,4 +1,7 @@
+from telethon import TelegramClient
+
 import kocherga.config
+
 API_ID = kocherga.config.config()['telegram']['core_api']['api_id']
 API_HASH = kocherga.config.config()['telegram']['core_api']['api_hash']
 SESSION = kocherga.config.config()['telegram']['core_api']['session_file']
