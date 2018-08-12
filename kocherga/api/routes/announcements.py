c12ee51fedfd4f793c4a0c6966b281cd0d55982f
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-12 17:04:38 +0300

    fix /screenshot/error

--- kocherga/api/routes/announcements.py
+++ kocherga/api/routes/announcements.py
@@ -128,4 +128,4 @@ def r_schedule_weekly_image():
 @bp.route("/screenshot/error", methods=["GET"])
 def r_last_screenshot():
     filename = image_storage.screenshot_file("error")
-    return send_file(filename, mimetype="image/png")
+    return send_file(filename)
