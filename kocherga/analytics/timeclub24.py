13f88f6bda390d0707b578c388eaa7c9f2d73b20
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-12 17:56:14 +0300

    timeclub24 visitors; kocherga.telegram.core_api

---
+++ kocherga/analytics/timeclub24.py
@@ -0,0 +1,38 @@
+from telethon import TelegramClient
+import socks
+import time
+
+import kocherga.db
+import datetime
+
+import kocherga.telegram.core_api
+
+TIMECLUB_BOT = 'timeclub24_bot'
+
+class Timeclub24Visitors(kocherga.db.Base):
+    __tablename__ = "timeclub24_visitors"
+    __table_args__ = (UniqueConstraint("ts", "venue"),)
+
+    ts = Column(DateTime, default=datetime.now)
+    venue = Column(String(100), nullable=False)
+    visitors = Column(Integer)
+
+class Importer(kocherga.importer.base.FullImporter):
+    def __init__(self):
+        self.client = kocherga.telegram.core_api.get_client()
+
+    def do_full_import(self, session):
+        self.client.send_message(TIMECLUB_BOT, 'Загруженность клубов')
+        time.sleep(3)
+        message = self.client.get_messages(TIMECLUB_BOT)[0].message
+
+        parts = message.split('\n')
+        ts = datetime.now()
+        for part in parts:
+            (venue, visitors) = re.match(r'(.*): (\d+)$', part).groups()
+
+            session.add(Timeclub24Visitors(
+                ts=ts,
+                venue=venue,
+                visitors=int(visitors),
+            ))
---
+++ kocherga/telegram/core_api.py
@@ -0,0 +1,9 @@
+import kocherga.config
+API_ID = kocherga.config.config()['telegram']['core_api']['api_id']
+API_HASH = kocherga.config.config()['telegram']['core_api']['api_hash']
+SESSION = kocherga.config.config()['telegram']['core_api']['session_file']
+
+def get_client():
+    client = TelegramClient(SESSION, API_ID, API_HASH)
+    client.start()
+    return client
