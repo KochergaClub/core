e14d100c5266b97dcb51c4e9b7e6857da775f040
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-20 00:20:21 +0300

    wrap worker.py jobs and reset db session

--- scripts/worker.py
+++ scripts/worker.py
@@ -6,20 +6,27 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 import kocherga.events.vk
 import kocherga.vk.tools
+from kocherga.db import Session
+
+def job_wrapper(func):
+    # Without this the connection can cache the old data if transaction isolation level is set to REPEATABLE READ.
+    # (See https://dev.mysql.com/doc/refman/8.0/en/innodb-transaction-isolation-levels.html for details.)
+    Session.remove()
+    func()
 
 def main():
     scheduler = BlockingScheduler()
 
     scheduler.add_job(
-        func=kocherga.events.vk.update_widget,
-        trigger="interval",
+        func=job_wrapper(kocherga.events.vk.update_widget),
+        trigger='interval',
         name='update_vk_widget',
         minutes=1,
     )
 
     scheduler.add_job(
-        func=kocherga.vk.tools.update_cover,
-        trigger="interval",
+        func=job_wrapper(kocherga.vk.tools.update_cover),
+        trigger='interval',
         name='update_vk_cover',
         minutes=2,
     )
