77715261d325d2d9330861b5be1ca1d2d9bc5d4d
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-24 05:06:22 +0300

    rewrite zadarma importer

--- kocherga/zadarma.py
+++ kocherga/zadarma.py
@@ -4,10 +4,15 @@ logger = logging.getLogger(__name__)
 
 import csv
 import requests
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from datetime import datetime, timedelta
 import enum
 import io
+import hashlib
+import hmac
+import base64
+import urllib
+import itertools
 
 from typing import DefaultDict, Dict, Iterator, List
 
@@ -18,9 +23,7 @@ import kocherga.secrets
 import kocherga.watchmen
 import kocherga.importer.base
 
-from sqlalchemy import Column, Integer, String, Enum
-
-SECRET = kocherga.secrets.plain_secret("zadarma_api_key")
+from sqlalchemy import Column, Integer, String, Enum, DateTime, Boolean
 
 
 class CallType(enum.Enum):
@@ -28,112 +31,105 @@ class CallType(enum.Enum):
     outcoming = 2
 
     @classmethod
-    def from_str(cls, s: str) -> "CallType":
-        return {"in": cls.incoming, "out": cls.outcoming}[s]
-
-
-class Disposition(enum.Enum):
-    answered = 1
-    no_answer = 2
-    busy = 3
-    call_failed = 4
-    parse_failed = 5
-
-    @classmethod
-    def from_str(cls, s: str) -> "Disposition":
-        s = s.replace(" ", "_")
-        return cls[s]
+    def from_api_data(cls, data) -> "CallType":
+        if data['pbx_call_id'].startswith('in_'):
+            return cls.incoming
+        if data['pbx_call_id'].startswith('out_'):
+            return cls.outcoming
+        raise Exception(f"Can't detect call type by data {data}")
 
 
 class Call(kocherga.db.Base):
     __tablename__ = "zadarma_calls"
+
     call_id = Column(String(100), primary_key=True)
+    pbx_call_id = Column(String(100), index=True, nullable=False)
+
+    ts = Column(DateTime, nullable=False)
     call_type = Column(Enum(CallType))
-    internal_number = Column(String(100))
-    caller_number = Column(String(100))
-    destination_number = Column(String(100))
-    disposition = Column(Enum(Disposition))
-    ts = Column(Integer)
-    wait_seconds = Column(Integer)
+
+    disposition = Column(String(40))
+
+    clid = Column(String(100))
+    destination = Column(String(20))
+    sip = Column(String(100))
+
     seconds = Column(Integer)
+    is_recorded = Column(Boolean)
+
     watchman = Column(String(100))
 
     @classmethod
-    def from_csv_rows(cls, rows: List[Dict[str, str]]) -> "Call":
-        row1 = rows[0]
+    def from_api_data(cls, data) -> "Call":
+        args = {}
+        for arg in ('call_id', 'pbx_call_id', 'disposition', 'clid', 'sip', 'seconds'):
+            args[arg] = data[arg]
+
         call = Call(
-            call_id=row1["call_id"],
-            call_type=CallType.from_str(row1["call_type"]),
-            caller_number=row1["clid"],
-            destination_number=row1["destination"],
-            internal_number=row1["internal_number"],
-            disposition=Disposition.from_str(row1["disposition"]),
-            ts=datetime.strptime(row1["date"], "%Y-%m-%d %H:%M:%S").timestamp(),
-            seconds=int(row1["seconds"]),
+            ts=datetime.strptime(data['callstart'], "%Y-%m-%d %H:%M:%S"),
+            call_type=CallType.from_api_data(data),
+            is_recorded=(data['is_recorded'] == 'true'),
+            destination=str(data['destination']),
+            **args,
         )
 
-        if len(rows) > 2:
-            call.disposition = Disposition.parse_failed
+        #if len(rows) == 2:
+        #    row2 = rows[1]
+        #    assert row2["call_id"] == row1["call_id"]
+        #    if call.call_type != CallType.incoming:
+        #        raise Exception(
+        #            "Can't parse two-rows format for anything except incoming calls"
+        #        )
+        #    call.internal_number = row2["internal_number"]
 
-        if len(rows) == 2:
-            row2 = rows[1]
-            assert row2["call_id"] == row1["call_id"]
-            if call.call_type != CallType.incoming:
-                raise Exception(
-                    "Can't parse two-rows format for anything except incoming calls"
-                )
-            call.internal_number = row2["internal_number"]
-
-            call.wait_seconds = call.seconds
-            call.seconds = row2["seconds"]
+        #    call.wait_seconds = call.seconds
+        #    call.seconds = row2["seconds"]
 
         return call
 
 
-def api_text_to_row_groups(text: str) -> Iterator[List[Dict[str, str]]]:
-    if text.startswith("Вы уже использовали"):
-        raise Exception(f"Temporarily banned by API: {text}")
-
-    # Arrayid is a weird header for some older requests but they seem fine otherwise
-    if not text.startswith("id;call_type") and not text.startswith("Arrayid;call_type"):
-        raise Exception(f"Unexpected response {text}")
-
-    reader = csv.DictReader(io.StringIO(text), delimiter=";")
-
-    rows_by_call_id = defaultdict(list)  # type: DefaultDict[str, List[Dict[str, str]]]
-    for row in reader:
-        rows_by_call_id[row["call_id"]].append(row)
-
-    logger.info(f"Got {len(rows_by_call_id)} rows")
-
-    for (call_id, rows) in rows_by_call_id.items():
-        yield rows
-
-
-def fetch_csv_row_groups(
-    from_dt: datetime, to_dt: datetime
-) -> Iterator[List[Dict[str, str]]]:
-    logger.info(f"Fetching from {from_dt} to {to_dt}")
-    api_params = {
-        "secret": SECRET,
-        "start": from_dt.strftime("%Y%m%d%H%M%S"),
-        "end": to_dt.strftime("%Y%m%d%H%M%S"),
-        "format": "new",
-    }
-    r = requests.get("https://my.zadarma.com/mypbx/stat/export", params=api_params)
-
-    r.raise_for_status()
-
-    return api_text_to_row_groups(r.text)
-
-
 def fetch_calls(from_dt: datetime, to_dt: datetime) -> Iterator[Call]:
-    for rows in fetch_csv_row_groups(from_dt, to_dt):
-        yield Call.from_csv_rows(rows)
+    dt_format = '%Y-%m-%d %H:%M:%S'
+    params=OrderedDict(
+        sorted(
+            {
+                'start': from_dt.strftime(dt_format),
+                'end': to_dt.strftime(dt_format),
+                'version': 2,
+                'format': 'json',
+            }.items()
+        )
+    )
+
+    credentials = kocherga.secrets.json_secret('zadarma_credentials')
+    key = credentials['key']
+
+    params_str = urllib.parse.urlencode(params)
+    hmac_value = hmac.new(
+        credentials['secret'].encode('utf-8'),
+        (
+            '/v1/statistics/pbx/'
+            + params_str
+            + hashlib.md5(params_str.encode('utf-8')).hexdigest()
+        ).encode('utf-8'),
+        hashlib.sha1
+    ).hexdigest()
+    signature = base64.b64encode(hmac_value.encode('utf-8')).decode('utf-8')
+
+    r = requests.get(
+        'https://api.zadarma.com/v1/statistics/pbx/?' + params_str,
+        headers={
+            'Authorization': f'{key}:{signature}'
+        }
+    )
+    r.raise_for_status()
+    itertools.groupby(r.json()['stats'])
+    for item in r.json()['stats']:
+        yield Call.from_api_data(item)
 
 
 def fetch_all_calls(
-    from_dt=datetime(2015, 9, 1, tzinfo=TZ), to_dt=None
+    from_dt=datetime(2016, 4, 1, tzinfo=TZ), to_dt=None
 ) -> Iterator[Call]:
     api_requests = 0
     for (chunk_from_dt, chunk_to_dt) in kocherga.datetime.date_chunks(
@@ -149,7 +145,7 @@ def fetch_all_calls(
         # We shouldn't make too many API requests in a row - we'll get banned.
         # This early break helps the importer to finish the full import in several portions.
         api_requests += 1
-        if api_requests >= 3:
+        if api_requests >= 10:
             break
 
 
@@ -164,12 +160,12 @@ class Importer(kocherga.importer.base.IncrementalImporter):
         schedule = kocherga.watchmen.load_schedule()
 
         for call in fetch_all_calls(from_dt, to_dt):
-            watchman = schedule.watchman_by_dt(datetime.fromtimestamp(call.ts, TZ))
+            watchman = schedule.watchman_by_dt(datetime.fromtimestamp(call.ts.timestamp(), TZ))
             call.watchman = watchman
             session.merge(call)
             last_call = call
 
         if last_call:
-            return datetime.fromtimestamp(last_call.ts, TZ)
+            return datetime.fromtimestamp(last_call.ts.timestamp(), TZ)
         else:
             return from_dt
