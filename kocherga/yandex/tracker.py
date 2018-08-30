89ed18d2ce128f23ad32ad359335fc7f89d0b2ca
Vyacheslav Matyukhin <me@berekuk.ru>
2018-08-30 19:31:56 +0300

    more tracker code

--- kocherga/yandex/tracker.py
+++ kocherga/yandex/tracker.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import re
 import requests
 import dateutil.parser
@@ -19,15 +22,53 @@ gl2tracker_users = {
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.000+03:00"
 
-def import_gl_note(gl_note, gl_issue, yandex_issue_id):
-    created_by = gl2tracker_users.get(gl_issue.author['username'])
-    if not created_by:
-        text += f"\n\n(Автор: {gl_issue.author['username']})"
+class ConflictException(Exception):
+    pass
 
+def api_call(method, url, data={}):
+    if method == "GET":
+        r = requests.get(
+            f"{ROOT}/{url}",
+            headers={
+                'Authorization': f'OAuth {token()}',
+                'X-Org-ID': str(ORG_ID),
+            },
+        )
+    elif method == "POST":
+        r = requests.post(
+            f"{ROOT}/{url}",
+            headers={
+                'Authorization': f'OAuth {token()}',
+                'X-Org-ID': str(ORG_ID),
+            },
+            json=data,
+        )
+
+    if r.status_code == 409:
+        raise ConflictException()
+    if r.status_code >= 400:
+        logger.warning(data)
+        raise Exception(f"Error: {r.status_code} {r.reason}\n\n{r.text}")
+    r.raise_for_status()
+
+    return r.json()
+
+def import_gl_note(gl_note, gl_issue, yandex_issue_id):
+    created_by = gl2tracker_users.get(gl_note.author['username'])
+    text = gl_note.body
+    if not created_by:
+        text += f"\n\n(Автор: {gl_note.author['username']})"
+        created_by = 'info'
+
+    created_dt = dateutil.parser.parse(gl_note.created_at)
+    issue_updated_dt = dateutil.parser.parse(gl_issue.attributes["updated_at"])
+    if created_dt > issue_updated_dt:
+        created_dt = issue_updated_dt
+
+    logger.info('note dt: ' + str(created_dt))
     payload = {
-        'text': gl_note.body,
-        'createdAt': "2017-08-29T12:34:41.740+0000",
-        'createdAt': dateutil.parser.parse(gl_note.created_at).strftime(DATE_FORMAT),
+        'text': text,
+        'createdAt': created_dt.strftime(DATE_FORMAT),
         'createdBy': created_by,
     }
 
@@ -36,21 +77,10 @@ def import_gl_note(gl_note, gl_issue, yandex_issue_id):
     #     payload['updatedAt'] = dateutil.parser.parse(gl_note.updated_at).strftime(DATE_FORMAT)
     #     payload['updatedBy'] = updated_by
 
-    r = requests.post(
-        f'{ROOT}/issues/{yandex_issue_id}/comments/_import',
-        headers={
-            'Authorization': f'OAuth {token()}',
-            'X-Org-ID': str(ORG_ID),
-        },
-        json=payload
-    )
-    if r.status_code != 409:
-        if r.status_code != 200:
-            print(r.json())
-            print(payload)
-        r.raise_for_status()
+    api_call('POST', f'issues/{yandex_issue_id}/comments/_import', payload)
 
-def import_gl_issue(gl_issue, target_queue='TESTTT'):
+IMPORT_UNIQUE_KEY = 'gitlab4'
+def import_gl_issue(gl_issue, target_queue):
 
     def gl_wiki_link(url):
         name = re.search(r'(\w+/issues/\d+)$', url).group(1)
@@ -59,31 +89,31 @@ def import_gl_issue(gl_issue, target_queue='TESTTT'):
     created_dt=dateutil.parser.parse(gl_issue.attributes["created_at"])
     updated_dt=dateutil.parser.parse(gl_issue.attributes["updated_at"])
 
-    r = requests.post(
-        f'{ROOT}/issues/_import',
-        headers={
-            'Authorization': f'OAuth {token()}',
-            'X-Org-ID': str(ORG_ID),
-        },
-        json={
+    try:
+        logger.info(gl_issue.id)
+        request_body = {
             'queue': target_queue,
             'summary': gl_issue.title,
             'createdAt': created_dt.strftime(DATE_FORMAT),
-            'createdBy': gl2tracker_users.get(gl_issue.author['username'], 'slava'),
+            'createdBy': gl2tracker_users.get(gl_issue.author['username'], 'info'),
             'updatedAt': updated_dt.strftime(DATE_FORMAT),
-            'updatedBy': gl2tracker_users.get(gl_issue.author['username'], 'slava'), # this is incorrect
+            'updatedBy': gl2tracker_users.get(gl_issue.author['username'], 'info'),
             'description': f'{gl_wiki_link(gl_issue.web_url)}\n\n{gl_issue.description}',
-            'unique': f'gitlab2-{gl_issue.id}',
+            'tags': [s.replace(' ', '_').replace(',', '_') for s in gl_issue.attributes.get('labels', [])],
+            'unique': f'{IMPORT_UNIQUE_KEY}-{gl_issue.id}',
+            'status': 8 if gl_issue.state == 'closed' else 1,
         }
-    )
-    if r.status_code != 409:
-        if r.status_code != 200:
-            print(r.json())
-        r.raise_for_status()
+        if gl_issue.attributes['assignee']:
+            request_body['assignee'] = gl2tracker_users.get(gl_issue.attributes['assignee']['username'], 'info')
+
+        result = api_call('POST', 'issues/_import', request_body)
 
         for gl_note in gl_issue.notes.list(all=True, as_list=False):
-            import_gl_note(gl_note, gl_issue, r.json()['key'])
+            import_gl_note(gl_note, gl_issue, result['key'])
+
+    except ConflictException:
+        pass # whatever
 
 def import_main_tasks():
-    for gl_issue in kocherga.gitlab.main_project().issues.list(all=True, as_list=False):
-        import_gl_issue(gl_issue)
+    for gl_issue in kocherga.gitlab.main_project().issues.list(all=True, as_list=False, order_by='created_at', sort='asc'):
+        import_gl_issue(gl_issue, 'TESTC')
