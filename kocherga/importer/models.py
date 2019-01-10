2d6b157fc37b20146ac14a1a811893b827efeaae
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-10 17:52:05 +0300

    migrations for importer

--- kocherga/importer/models.py
+++ kocherga/importer/models.py
@@ -3,11 +3,16 @@ from django.db import models
 from datetime import datetime
 from kocherga.config import TZ
 
+from typing import Optional
+
 class State(models.Model):
     name = models.CharField(primary_key=True, max_length=100)
     until_dt = models.DateTimeField(null=True)
     last_dt = models.DateTimeField(null=True)
     last_exception = models.CharField(max_length=1024, null=True)
+
+    class Meta:
+        db_table = 'importers_state'
 
 def dt_now():
     return datetime.now(TZ)
@@ -17,3 +22,6 @@ class LogEntry(models.Model):
     start_dt = models.DateTimeField(default=dt_now)
     end_dt = models.DateTimeField(null=True)
     exception = models.TextField(null=True)
+
+    class Meta:
+        db_table = 'importers_log_entry'
