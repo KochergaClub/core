9a9b47f67837a4fc263741b17570ea9b03164403
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-13 03:20:45 +0300

    remove useless __init__.py files

--- kocherga/__init__.pyi
+++
@@ -1,3 +0,0 @@
-# Stub file for mypy.
-# It's important that this file is .pyi, not .py - kocherga.* is a namespace package because some kocherga.* code resides in other repositories (e.g. ludwig).
-# See also: https://github.com/python/mypy/issues/1645
