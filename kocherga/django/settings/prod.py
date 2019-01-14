0d9db0ffecbfa4fdcc2d0932d8356da252da0b55
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-14 21:55:55 +0300

    prod security settings

--- kocherga/django/settings/prod.py
+++ kocherga/django/settings/prod.py
@@ -12,6 +12,20 @@ sentry_sdk.init(
 
 DEBUG = False
 
+# Doesn't matter, really, since we use nginx as a reverse proxy.
+ALLOWED_HOSTS = ['*']
+
+# Security
+
+# TODO - deploy these; but for now we have HTTP-only iacsv.kocherga.club
+# SECURE_HSTS_SECONDS = 86400 # TODO - increase
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Kocherga settings
 KOCHERGA_TEAM_SPREADSHEET_KEY = '1mTt7kI9A3CNzhDSKBcyY-PNGd5E2ooBRaGcqXN-t_7g'
 
