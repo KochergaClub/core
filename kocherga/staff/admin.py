5021a7c54c0d1beeb80ad685c0e578b6e6fad12e
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-16 00:28:56 +0300

    LogEntryAdmin

--- kocherga/staff/admin.py
+++ kocherga/staff/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
 from .models import Member, AltEmail
 
@@ -14,3 +15,21 @@ class MemberAdmin(admin.ModelAdmin):
     inlines = [
         AltEmailInline,
     ]
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
