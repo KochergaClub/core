e538a8834dc22055c00d88daa2a4f3e6d3bdc479
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-15 16:22:56 +0300

    new tests naming; tests for x-frame-options

--- tests/api/templater_test.py
+++ tests/api/templater_test.py
@@ -6,6 +6,7 @@ def test_html(client):
         '/api/templater/mailchimp/html?start_date=2017-03-01&end_date=2017-03-08',
     )
     assert res.status_code == 200
+    assert res.get('X-Frame-Options') is None
 
 
 def test_list(client, kocherga_auth_header):
