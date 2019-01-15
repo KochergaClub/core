df75b970b7a1ae4e306468ef42cbe305a9c704ca
Vyacheslav Matyukhin <me@berekuk.ru>
2019-01-16 02:00:19 +0300

    importer admin

---
+++ kocherga/importer/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import State, LogEntry
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_dt', 'until_dt', 'last_exception')
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    pass
