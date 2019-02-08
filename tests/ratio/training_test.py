1de61faf34166fc9162f6354447dae73a92fb287
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-08 13:30:34 +0300

    training.total_income tests

--- tests/ratio/training_test.py
+++ tests/ratio/training_test.py
@@ -6,3 +6,9 @@ def test_tickets_count_zero(training):
 
 def test_tickets_count(training, ticket):
     assert training.tickets_count() == 1
+
+def test_total_income_zero(training):
+    assert training.total_income() == 0
+
+def test_total_income(training, ticket):
+    assert training.total_income() == 15000
