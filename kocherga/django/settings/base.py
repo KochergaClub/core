f910ee6106b085301a9ce9ac2f18c829bcce6806
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-08 16:39:30 +0300

    get rid of bootstrap before it infects everything

--- kocherga/django/settings/base.py
+++ kocherga/django/settings/base.py
@@ -168,6 +168,9 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 ###### Kocherga settings ######
