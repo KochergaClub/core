2b85ed3315cb3a92902a927086d180d798f033ae
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-09 23:51:38 +0300

    my: some cm stats

--- kocherga/my/views.py
+++ kocherga/my/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MainView(LoginRequiredMixin, View):
     def get(self, request):
+        customer = request.user.customer if hasattr(request.user, 'customer') else None
         return render(request, 'my/index.html', {
             'email': request.user.email,
             'is_staff': request.user.is_staff,
-            'customer': request.user.customer if hasattr(request.user, 'customer') else None,
+            'customer': customer,
+            'orders': list(customer.orders()) if customer else []
         })
