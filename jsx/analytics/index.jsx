46431766a69d6855dfb415afa4120e1a66977b72
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-14 18:03:26 +0300

    /team/analytics page

---
+++ jsx/analytics/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Page from '../components/Page';
+
+const BOVStatCard = ({ bovStat }) => (
+  <div>
+    <h3>{bovStat.date}</h3>
+    <p>Всего карт: {bovStat.count}</p>
+    <p>Всего доход: {bovStat.total_income}</p>
+  </div>
+);
+
+export default ({ bov_stats }) => (
+  <Page title="Аналитика Кочерги">
+    {JSON.stringify(bov_stats)}
+    <h1>Аналитика Кочерги</h1>
+    <ul>
+      <li>
+        <a href="https://metabase.team.kocherga.club">Metabase</a>
+      </li>
+      <li>
+        <a href="https://wiki.team.kocherga.club/Категория:Аналитика">
+          О аналитике на вики
+        </a>
+      </li>
+    </ul>
+    <h2>Большие открытые встречи</h2>
+    {bov_stats.map((bovStat, i) => <BOVStatCard key={i} bovStat={bovStat} />)}
+  </Page>
+);
---
+++ kocherga/analytics/views.py
@@ -0,0 +1,28 @@
+from django.contrib.admin.views.decorators import staff_member_required
+
+from kocherga.django.react import react_render
+
+from kocherga.cm.models import Customer
+
+from datetime import datetime
+
+@staff_member_required
+def index(request):
+    cohort_dates = [
+        '2019-01-07',
+        '2019-02-07',
+    ]
+
+    BOV_stats = []
+    for date_str in cohort_dates:
+        customers = Customer.objects.from_date(datetime.strptime(date_str, '%Y-%m-%d').date())
+        total_income = sum(c.total_spent for c in customers)
+        BOV_stats.append({
+            'date': date_str,
+            'count': customers.count(),
+            'total_income': total_income,
+        })
+
+    return react_render(request, 'analytics/index.jsx', {
+        'bov_stats': BOV_stats,
+    })
