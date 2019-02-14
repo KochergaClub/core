d8709f13124f14c0d637e679c477df38370650b6
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-14 15:59:40 +0300

    comment for auth app's label

--- kocherga/auth/apps.py
+++ kocherga/auth/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 class KochergaAuthConfig(AppConfig):
     name = 'kocherga.auth'
+    # Our typical convention for label would be 'auth' (which is implicit).
+    # But we want 'kocherga_auth_user' db table so that it's clearer that this table is kocherga-related.
     label = 'kocherga_auth'
     verbose_name = 'Пользователи'
