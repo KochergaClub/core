012b5443230b98a3fdca1ca177cc6e5ad22c764d
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-18 12:40:56 +0300

    another deleted cm user

--- kocherga/cm/importer.py
+++ kocherga/cm/importer.py
@@ -69,7 +69,7 @@ def load_users():
     users = []
 
     # needed for OrderLogEntries
-    for i, name in enumerate(["Мальбург", "Андрей Ершов", "Макс", "Лев", "Женя", "Лена", "Маша", "Ринат", "Роман", "Филипп Б.", "Стася"]):
+    for i, name in enumerate(["Мальбург", "Андрей Ершов", "Макс", "Лев", "Женя", "Лена", "Маша", "Ринат", "Роман", "Филипп Б.", "Стася", "Юрий"]):
         deleted_user = User(id=-i, name=name, login=name, level="deleted")
         users.append(deleted_user)
 
