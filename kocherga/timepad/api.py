706d7b3a7bbb3784b732d0bb57d9758c1013d6dc
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-22 20:26:04 +0300

    kocherga.timepad.api module

---
+++ kocherga/timepad/api.py
@@ -0,0 +1,41 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from django.conf import settings
+
+import requests
+
+BASE_URL = "https://api.timepad.ru/v1"
+
+
+def token():
+    return settings.KOCHERGA_TIMEPAD_TOKEN
+
+
+def api_call(method, url, payload={}):
+    url = f"{BASE_URL}/{url}.json"
+    query_params = {
+        'token': token()
+    }
+    if method == "GET":
+        r = requests.get(
+            url,
+            params={
+                **query_params,
+                **payload,
+            },
+        )
+    elif method == "POST":
+        r = requests.post(
+            url,
+            params=query_params,
+            json=payload,
+        )
+    else:
+        raise Exception(f"Unknown method {method}")
+
+    if r.status_code >= 400:
+        raise Exception(f"Error: {r.status_code} {r.reason}\n\n{r.text}")
+    r.raise_for_status()
+
+    return r.json()
