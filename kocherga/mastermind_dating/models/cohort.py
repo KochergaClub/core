88876fb81c2cd04fb53c495109ab3a7718f98bb3
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-23 22:04:17 +0300

    send_invite_emails method

--- kocherga/mastermind_dating/models/cohort.py
+++ kocherga/mastermind_dating/models/cohort.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.mail import send_mail
+
 from kocherga.events.models import Event as KchEvent
 
 from .user import User as MmUser
 
 class Cohort(models.Model):
     event = models.OneToOneField(KchEvent, on_delete=models.CASCADE, related_name='+', blank=True, null=True)
+    sent_emails = models.BooleanField(default=False)
 
     def populate_from_event(self):
         if not self.event:
@@ -16,3 +19,17 @@ class Cohort(models.Model):
                 cohort=self,
             )
             # TODO - remove everyone else (people can cancel their registrations)
+
+    def send_invite_emails(self, force=False):
+        if self.sent_emails and not force:
+            raise Exception("Already sent invite emails")
+
+        for user in self.users.all():
+            send_mail(
+                'Регистрация на мастермайнд-дейтинг',
+                f"Мастермайнд-дейтинг в Кочерге уже завтра!\n\nВот ссылка на телеграм-бота, обязательно зарегистрируйтесь через него: {user.telegram_link()}.\n\nЕсли ссылка почему-то не работает, найдите в телеграме бота @KochergaMastermindBot и напишите ему эту строчку:\n/start {str(user.generate_token(), 'utf-8')}\n\nПожалуйста, сделайте это до начала мероприятия.",
+                'Кочерга <info@kocherga-club.ru>',
+                [user.user.email],
+            )
+        self.sent_emails = True
+        self.save()
