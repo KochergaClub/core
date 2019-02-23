fa231dbb9a38219f34c163480e2a4ad7a8847901
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-23 18:57:24 +0300

    event admin defines search_field

--- kocherga/events/admin.py
+++ kocherga/events/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Event, EventPrototype
 
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
 admin.site.register(EventPrototype)
