c0b55f158768570211e649e5b8903e04aa02ee2f
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-23 14:14:09 +0300

    mm bot: Cohort.populate_from_event (WIP)

--- kocherga/mastermind_bot/models/cohort.py
+++ kocherga/mastermind_bot/models/cohort.py
@@ -3,3 +3,15 @@ from kocherga.events.models import Event as KchEvent
 
 class Cohort(models.Model):
     event = models.OneToOneField(KchEvent, on_delete=models.CASCADE, related_name='+', blank=True, null=True)
+
+    objects = CohortManager()
+
+    def populate_from_event(self):
+        if not self.event:
+            raise Exception("Cohort doesn't have an associated event")
+
+        timepad_link = self.event.posted_timepad
+        if not timepad_link:
+            raise Exception("Event is not posted to timepad")
+
+        raise NotImplementedError("TODO")
