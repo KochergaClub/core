62fceaff729137046840350148eaf3d2301da35e
Vyacheslav Matyukhin <me@berekuk.ru>
2019-02-24 18:50:25 +0300

    move solver code to cohort; fixes

--- kocherga/mastermind_dating/models/cohort.py
+++ kocherga/mastermind_dating/models/cohort.py
@@ -1,8 +1,18 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from django.db import models
+
+import subprocess
+import typing
+import json
+import random
 
 from kocherga.events.models import Event as KchEvent
 
-from .user import User as MmUser
+from .user import User
+from .vote import Vote
+from .group import Group
 
 from .. import rpc
 
@@ -14,9 +24,9 @@ class Cohort(models.Model):
         if not self.event:
             raise Exception("Cohort doesn't have an associated event")
 
-        for user in self.event.registered_users():
-            MmUser.objects.create(
-                user=user,
+        for kocherga_user in self.event.registered_users():
+            User.objects.create(
+                user=kocherga_user,
                 cohort=self,
             )
             # TODO - remove everyone else (people can cancel their registrations)
@@ -34,3 +44,90 @@ class Cohort(models.Model):
     def broadcast_solution(self):
         manager = rpc.get_client()
         manager.broadcast_solution(self.id)
+
+    def run_solver(self):
+        yall: typing.List[User] = list(self.users.all())
+
+        users: typing.List[User] = []
+        votes = []
+        for u in yall:
+            uvotes = list(Vote.objects.filter(who=u).iterator())
+            if len(uvotes) == 0:
+                logger.info(
+                    f"User {u.telegram_uid}({u.user.email}) is a {random.choice(['mushroom', 'cow', 'goat', 'user'])} and "
+                    f"didn't vote. Will be skipped."
+                )
+                continue
+            if len(uvotes) < len(yall) - 1:
+                logger.info(
+                    f"User {u.telegram_uid}({u.user.email}) is a {random.choice(['mushroom', 'cow', 'goat', 'user'])} and "
+                    f"skipped some ({len(yall) - 1 - len(uvotes)}) users in voting."
+                )
+            else:
+                logger.info(
+                    f"User {u.telegram_uid}({u.user.email}) [OK]"
+                )
+            votes += uvotes
+            users.append(u)
+
+        votes = [vote for vote in votes if vote.whom in users]
+        n = len(users)
+        if len(votes) != n ** 2 - n:
+            logger.info("Not enough votes to do solving")
+            return None
+
+        users.sort(key=lambda u: u.telegram_uid)
+        votes.sort(key=lambda v: v.whom.telegram_uid)
+        votes.sort(key=lambda v: v.who.telegram_uid)
+
+        userlist = [users]
+
+        def prepare_data():
+            builder = []
+            for i in range(n):
+                builder.append('|\n')
+                for j in range(n):
+                    if i == j:
+                        vote = 0
+                    else:
+                        vote = votes[(i * n + j) - ((i * n + j) // (n + 1) + 1)].how
+                    builder.append(f"{['Y', 'O', 'N'][vote]},")
+            builder.append('|')
+
+            unames = ",".join(map(lambda a: f"p{a}", range(n)))
+            dataset = "".join(builder)
+
+            return (
+                    f"people={{{unames}}};"
+                    "max_p_per_group = 5;"
+                    f"max_Ns = {n * n};"
+                    f"marks=[{dataset}];"
+                )
+
+        with open("data.dzn", mode="w") as f:
+            f.write(prepare_data())
+
+        logger.info("Starting solver.")
+        subprocess.run(["minizinc/minizinc", "mm-bot-model/model.mzn", "data.dzn"], stdout=open("solution.json", mode='w'))
+        solution = json.load(open("solution.json", mode="r"))
+        logger.info(f"Got solution: {solution}")
+
+        logger.info(f"Saving solution to db groups")
+        ptg = solution["people_to_groups"]
+        assert len(ptg) == len(users)
+        passoc = dict()
+
+        for i in range(len(ptg)):
+            passoc[users[i]] = ptg[i]
+
+        group_names = set(solution["people_to_groups"])
+        groups = {group: [u for u in users if passoc[u] == group] for group in group_names}
+
+        for group_id in groups.keys():
+            group: typing.List[User] = groups[group_id]
+
+            db_group = Group.objects.get_empty()
+            for user in group:
+                user.group = db_group
+                user.save()
+
--- kocherga/mastermind_dating/solver.py
+++ kocherga/mastermind_dating/solver.py
@@ -1,119 +1,24 @@
 import asyncio
-import json
 import logging
-import os
-import random
-import subprocess
 import typing
 
 from aiogram import Bot
 from django.template.loader import render_to_string
 
-from . import models as db
+from . import models
 
 logging.basicConfig(level="DEBUG")
 log = logging.getLogger("mm-bot-solver")
 
 
-def run_solver(cohort: db.Cohort):
-    yall: typing.List[db.User] = \
-        [user for user in db.User.objects.filter(cohort=cohort).iterator()]
-
-    users: typing.List[db.User] = []
-    votes = []
-    for u in yall:
-        uvotes = list(db.Vote.objects.filter(who=u).iterator())
-        if len(uvotes) == 0:
-            log.info(
-                f"User {u.telegram_uid}({u.user.email}) is a {random.choice(['mushroom', 'cow', 'goat', 'user'])} and "
-                f"didn't vote. Will be skipped."
-            )
-            continue
-        if len(uvotes) < len(yall) - 1:
-            log.info(
-                f"User {u.telegram_uid}({u.user.email}) is a {random.choice(['mushroom', 'cow', 'goat', 'user'])} and "
-                f"skipped some ({len(yall) - 1 - len(uvotes)}) users in voting."
-            )
-        else:
-            log.info(
-                f"User {u.telegram_uid}({u.user.email}) [OK]"
-            )
-        votes += uvotes
-        users.append(u)
-
-    votes = [vote for vote in votes if vote.whom in users]
-    n = len(users)
-    if len(votes) != n ** 2 - n:
-        log.info("Not enough votes to do solving")
-        return None
-
-    users.sort(key=lambda u: u.telegram_uid)
-    votes.sort(key=lambda v: v.whom.telegram_uid)
-    votes.sort(key=lambda v: v.who.telegram_uid)
-
-    userlist = [users]
-
-    def prepare_data():
-        builder = []
-        for i in range(n):
-            builder.append('|\n')
-            for j in range(n):
-                if i == j:
-                    vote = 0
-                else:
-                    vote = votes[(i * n + j) - ((i * n + j) // (n + 1) + 1)].how
-                builder.append(f"{['Y', 'O', 'N'][vote]},")
-        builder.append('|')
-
-        unames = ",".join(map(lambda a: f"p{a}", range(n)))
-        dataset = "".join(builder)
-
-        return (
-                   f"people={{{unames}}};"
-                   "max_p_per_group = 5;"
-                   f"max_Ns = {n * n};"
-                   f"marks=[{dataset}];"
-               )
-
-    with open("data.dzn", mode="w") as f:
-        f.write(prepare_data())
-
-    log.info("Starting solver.")
-    subprocess.run(["minizinc/minizinc", "mm-bot-model/model.mzn", "data.dzn"], stdout=open("solution.json", mode='w'))
-    solution = json.load(open("solution.json", mode="r"))
-    log.info(f"Got solution: {solution}")
-
-    save_solution(solution, users)
-
-def save_solution(solution, users):
-    ptg = solution["people_to_groups"]
-    assert len(ptg) == len(users)
-    passoc = dict()
-
-    for i in range(len(ptg)):
-        passoc[users[i]] = ptg[i]
-
-    group_names = set(solution["people_to_groups"])
-    groups = {group: [u for u in users if passoc[u] == group] for group in group_names}
-
-    for group_id in groups.keys():
-        group: typing.List[db.User] = groups[group_id]
-
-        db_group = db.Group.objects.get_empty()
-        for user in group:
-            user.group = db_group
-            user.save()
-
-
 async def broadcast_solution(cohort_id: int, bot: Bot):
     log.info(f"Broadcasting solution")
 
-    cohort = db.Cohort.objects.get(pk=cohort_id)
+    cohort = models.Cohort.objects.get(pk=cohort_id)
 
     tasks: typing.List[typing.Awaitable] = []
 
-    log.info(f"Starting broadcast, final data: {groups}")
-    for user in db.User.objects.filter(cohort=cohort):
+    for user in models.User.objects.filter(cohort=cohort):
         if not user.group:
             log.warn('No group for user ' + user.telegram_uid)
             continue
