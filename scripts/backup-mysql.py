a4e7064db159cbdea911d312d49f1e41949047c5
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-23 00:48:04 +0300

    backup-mysql: kocherga db only, fixes

--- scripts/backup-mysql.py
+++ scripts/backup-mysql.py
@@ -10,28 +10,34 @@ import subprocess
 
 from django.conf import settings
 
-MYSQL_ROOT_PASSWORD='' # FIXME
 
 def main():
-    AWS_CONFIG = settings.KOCHERGA_BACKUPS_AWS_CREDENTIALS
     BUCKET_NAME = settings.KOCHERGA_BACKUPS_S3_BUCKET
 
-    logging.info('Backing up MySQL databases...')
+    logging.info('Backing up MySQL database...')
 
-    s3 = boto3.session.Session(**AWS_CONFIG).client('s3')
+    s3 = boto3.session.Session(
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    ).client('s3')
 
     tmp_dir = Path('/tmp') / 'mysql'
     tmp_dir.mkdir(exist_ok=True)
 
-    for db_name in sys.argv[1:]:
-        tmp_file = tmp_dir / f'{db_name}.gz'
+    DB = settings.DATABASES['default']
+    tmp_file = tmp_dir / f"{DB['NAME']}.gz"
 
-        logging.info(f'Backing up {db_name} to {tmp_file}')
-        subprocess.run(f'mysqldump -uroot --password={MYSQL_ROOT_PASSWORD} {db_name} | gzip >{tmp_file}', shell=True, check=True)
+    logging.info(f"Backing up {DB['NAME']} to {tmp_file}")
+    subprocess.run(
+        f"mysqldump -u{DB['USER']} --password={DB['PASSWORD']} --host={DB['HOST']} {DB['NAME']} | gzip >{tmp_file}",
+        shell=True,
+        check=True
+    )
 
-        logging.info(f'Uploading {tmp_file} to S3')
-        s3.upload_file(str(tmp_file), BUCKET_NAME, f'mysql/{db_name}.gz')
-        logging.info(f'MySQL DB {db_name} backup successful')
+    logging.info(f'Uploading {tmp_file} to S3')
+    s3.upload_file(str(tmp_file), BUCKET_NAME, f"mysql/{DB['NAME']}.gz")
+    logging.info(f'MySQL DB backup successful')
+
 
 if __name__ == '__main__':
     import sys, os, django
