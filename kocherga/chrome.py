41c9e9e57d422538f5cecd11e9e2d08f3a8d90c1
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-24 16:28:49 +0300

    browserless/chrome -> alpine-chrome

--- kocherga/chrome.py
+++ kocherga/chrome.py
@@ -1,5 +1,30 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import pyppeteer
+import requests
+
+ENDPOINT = 'chrome:3001'
 
 
+def get_websocket():
+    # We run headless chrome in docker container named `chrome`.
+    # ws://ENDPOINT is not enough - we have to get the actual websocket URL first.
+    r = requests.get(
+        f"http://{ENDPOINT}/json/version",
+        headers={
+            'Host': '',  # see https://github.com/cyrus-and/chrome-remote-interface/issues/340#issuecomment-384598817
+        }
+    )
+    r.raise_for_status()
+
+    ws = r.json()['webSocketDebuggerUrl']
+    ws = ws.replace('ws:///', f"ws://{ENDPOINT}/")
+    logger.info(f"Headless chrome's websocket: {ws}")
+    return ws
+
+
+# TODO - implement get_page() with context manager which cleans the resources correctly.
 async def get_browser():
-    return await pyppeteer.connect({'browserWSEndpoint': 'ws://chrome:3000'})
+    ws = get_websocket()
+    return await pyppeteer.connect({'browserWSEndpoint': ws})
