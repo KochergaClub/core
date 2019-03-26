9e33eed81f2c2c878da82dbc342ba4372f4a45da
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-26 16:09:45 +0300

    skip fb tests for now

--- tests/events/fb_test.py
+++ tests/events/fb_test.py
@@ -5,9 +5,13 @@ import kocherga.events.fb
 
 
 class TestCreate:
+    @pytest.mark.skip(reason="FB keeps banning our test users")
     @pytest.mark.asyncio
     async def test_create(self, event):
-        event.description += 'Чтобы узнавать о новых мероприятиях, вступайте в группу: {{Entity|fb_id=1083453805000382|vk_id=kocherga_club|name=Кочерга}}.'
+        event.description += (
+            "Чтобы узнавать о новых мероприятиях, вступайте в группу: "
+            "{{Entity|fb_id=1083453805000382|vk_id=kocherga_club|name=Кочерга}}."
+        )
 
         result = await kocherga.events.fb.create(
             event,
