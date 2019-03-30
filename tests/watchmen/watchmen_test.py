a68e4d6e6de701eb7aaa18fc1749e5ef15d9108b
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-30 03:43:56 +0300

    fix watchmen tests

--- tests/watchmen/watchmen_test.py
+++ tests/watchmen/watchmen_test.py
@@ -4,11 +4,20 @@ pytestmark = pytest.mark.usefixtures('db')
 import datetime
 from freezegun import freeze_time
 
+from django.core.management import call_command
+
 import kocherga.watchmen.tools
 import kocherga.watchmen.models
 
 
-def schedule(request, google_schedule, db_schedule):
+@pytest.fixture('session')
+def django_db_setup(django_db_setup, django_db_blocker):
+
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'tests/watchmen/fixtures.json')
+
+@pytest.fixture
+def schedule():
     return kocherga.watchmen.tools.load_schedule()
 
 
@@ -17,27 +26,24 @@ class TestSchedule:
         assert type(schedule) == kocherga.watchmen.models.Schedule
 
     def test_shifts_by_date(self, schedule):
-        by_date = schedule.shifts_by_date(datetime.date(2017, 12, 18))
+        by_date = schedule.shifts_by_date(datetime.date(2019, 3, 30))
 
         assert type(by_date) == dict
         assert by_date[kocherga.watchmen.models.Shift.MORNING] == 'Элиезер'
 
     def test_watchman_by_dt(self, schedule):
-        watchman = schedule.watchman_by_dt(datetime.datetime(2017, 12, 18, hour=15, minute=0))
+        watchman = schedule.watchman_by_dt(datetime.datetime(2019, 3, 30, hour=15, minute=0))
 
-        assert watchman == 'Скотт'
-
-    def test_save_to_db(self, db, schedule):
-        schedule.save_to_db()
+        assert watchman == 'Слава'
 
     def test_current_watchman(self, schedule):
-        with freeze_time('2017-12-20 10:00'):
+        with freeze_time('2019-03-30 10:00'):
             assert schedule.current_watchman() == 'Элиезер'
 
     def test_last_watchman(self, schedule):
-        with freeze_time('2017-12-20 01:00'):
-            assert schedule.last_watchman() == 'Слава'
+        with freeze_time('2019-03-31 01:00'):
+            assert schedule.last_watchman() == 'Робин'
 
     def test_nearest_watchman(self, schedule):
-        with freeze_time('2017-12-20 01:00'):
+        with freeze_time('2019-03-30 01:00'):
             assert schedule.nearest_watchman() == 'Элиезер'
