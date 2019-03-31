5b0a18b080e195bb1f9c0b8d06cd26c6096d73e4
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-31 12:51:36 +0300

    better staff pages

--- jsx/staff/member_page.tsx
+++ jsx/staff/member_page.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 import Page from '../components/Page';
+import { Column } from '@kocherga/frontkit';
+
+const Ex = styled.div`
+  background-color: #ddd;
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 0.8em;
+`;
 
 export default ({ member }) => (
   <Page title="Страница сотрудника" team>
-    <h1>
-      Страница сотрудника{' '}
-      <span style={{ color: member.color || 'black' }}>
-        {member.short_name}
-      </span>
-    </h1>
-    <h2>{member.full_name}</h2>
-    {member.slack_id && (
-      <a href={`https://kocherga.slack.com/messages/${member.slack_id}/`}>
-        Написать в slack
-      </a>
-    )}
-    {member.vk && <a href={member.vk}>VK</a>}
+    <Column centered gutter={20}>
+      <Column centered gutter={0}>
+        <h1>{member.full_name}</h1>
+        <h2 style={{ color: member.color || 'black' }}>{member.short_name}</h2>
+        {member.is_current || <Ex>Бывший сотрудник</Ex>}
+      </Column>
+      <Column>
+        {member.slack_id && (
+          <a href={`https://kocherga.slack.com/messages/${member.slack_id}/`}>
+            Написать в Slack
+          </a>
+        )}
+        {member.vk && <a href={member.vk}>Профиль VK</a>}
+      </Column>
+    </Column>
   </Page>
 );
--- kocherga/staff/serializers.py
+++ kocherga/staff/serializers.py
@@ -6,4 +6,4 @@ from . import models
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Member
-        fields = ('id', 'short_name', 'full_name', 'color', 'slack_id', 'vk')
+        fields = ('id', 'short_name', 'full_name', 'color', 'slack_id', 'vk', 'role', 'is_current')
