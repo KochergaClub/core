57789925a83578bd95b59e4c957442d7faaf94f2
Vyacheslav Matyukhin <me@berekuk.ru>
2019-03-31 20:54:30 +0300

    view_500 doesn't take exception param

--- jsx/components/Main.tsx
+++ jsx/components/Main.tsx
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 const Main = styled.main`
   max-width: 1080px;
-  margin: 0 auto;
+  margin-top: 0;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
 `;
 
 export default Main;
