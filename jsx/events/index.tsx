f646172d162beb10f8ed87f63affea48c8990da4
Vyacheslav Matyukhin <me@berekuk.ru>
2019-04-05 12:56:39 +0300

    update react-big-calendar; create events in db (WIP)

--- jsx/events/index.tsx
+++ jsx/events/index.tsx
@@ -12,15 +12,39 @@ import { EventDispatch } from './contexts';
 const startAccessor = (event: PublicEvent) => new Date(event.start);
 const endAccessor = (event: PublicEvent) => new Date(event.end);
 
-export default (props: { events: PublicEvent[] }) => {
+export default (props: { events: PublicEvent[]; csrfToken: string }) => {
   const [events, dispatch] = useReducer(reducer, props.events);
 
-  const newEvent = useCallback(({ start, end }: { start: Date; end: Date }) => {
-    dispatch({
-      type: 'CREATE',
-      payload: { start: moment(start), end: moment(end) },
-    });
-  }, []);
+  const newEvent = useCallback(
+    async ({ start, end }: { start: Date; end: Date }) => {
+      const response = await fetch('/api/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': props.csrfToken,
+        },
+        body: JSON.stringify({
+          start_ts: start.getTime() / 1000,
+          end_ts: end.getTime() / 1000,
+        }),
+      });
+
+      if (!response.ok) {
+        const json = await response.json(); // FIXME - this line can fail
+        window.alert(`Error: ${JSON.stringify(json)}`);
+        return;
+      }
+
+      const json = await response.json();
+      console.log(json);
+
+      dispatch({
+        type: 'CREATE',
+        payload: { start: moment(start), end: moment(end) },
+      });
+    },
+    []
+  );
 
   const resizeEvent = useCallback(
     ({ event, start, end }: { event: PublicEvent; start: Date; end: Date }) => {
