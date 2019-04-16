ac4f9bea9c7e59a63186b40503e4a91e30ff7453
Vyacheslav Matyukhin <me@berekuk.ru>
2019-04-17 00:04:43 +0300

    fix events tests

--- jsx/events/types.test.ts
+++ jsx/events/types.test.ts
@@ -1,8 +1,12 @@
 import { reducer } from './types';
 import moment from 'moment';
 
+const initialStore = {
+  events: [],
+};
+
 it('create from empty', () => {
-  const result = reducer([], {
+  const result = reducer(initialStore, {
     type: 'CREATE',
     payload: {
       start: moment('2020-05-01 19:00'),
@@ -11,22 +15,24 @@ it('create from empty', () => {
       id: 'abcd',
     },
   });
-  expect(result[0]).toBeTruthy();
-  expect(result.length).toEqual(1);
-  expect(result[0].title).toEqual('Some event');
+  expect(result.events[0]).toBeTruthy();
+  expect(result.events.length).toEqual(1);
+  expect(result.events[0].title).toEqual('Some event');
 });
 
 it('create from non-empty', () => {
   const result = reducer(
-    [
-      {
-        start: moment('2020-04-01 12:00').format(),
-        end: moment('2020-04-01 13:00').format(),
-        title: 'Older event',
-        id: 'oldold',
-        room: 'TODO',
-      },
-    ],
+    {
+      events: [
+        {
+          start: moment('2020-04-01 12:00').format(),
+          end: moment('2020-04-01 13:00').format(),
+          title: 'Older event',
+          id: 'oldold',
+          room: 'TODO',
+        },
+      ],
+    },
     {
       type: 'CREATE',
       payload: {
@@ -37,6 +43,6 @@ it('create from non-empty', () => {
       },
     }
   );
-  expect(result[0].title).toEqual('Older event');
-  expect(result[1].title).toEqual('Some event');
+  expect(result.events[0].title).toEqual('Older event');
+  expect(result.events[1].title).toEqual('Some event');
 });
