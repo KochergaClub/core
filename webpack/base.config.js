2cf1453fa33a1fc9c98edf7bb493fc5c59b9523c
Vyacheslav Matyukhin <me@berekuk.ru>
2019-04-26 03:00:06 +0300

    experimental cacheDirectory option

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -8,8 +8,15 @@ module.exports = {
       },
       {
         test: /\.tsx?$/,
-        use: 'babel-loader',
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Not mounted in Docker as volume -> won't cause collisions between front and back.
+            // This doesn't really affect the performance, need to investigate further.
+            cacheDirectory: '/tmp',
+          },
+        },
       },
     ],
   },
