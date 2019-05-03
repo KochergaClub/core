c37081db061fd0b403be7655dcf258422f263e80
Vyacheslav Matyukhin <me@berekuk.ru>
2019-05-03 03:09:00 +0300

    lint

--- kocherga/api/views/templater.py
+++ kocherga/api/views/templater.py
@@ -7,7 +7,7 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser
 
 import asyncio
 
