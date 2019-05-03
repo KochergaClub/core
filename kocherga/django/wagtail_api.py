746c3499ca66cae3034a2d6443a50a4fbf74271b
Vyacheslav Matyukhin <me@berekuk.ru>
2019-05-03 03:33:23 +0300

    fix wagtail views

--- kocherga/django/wagtail_api.py
+++ kocherga/django/wagtail_api.py
@@ -1,20 +1,24 @@
 from wagtail.api.v2.router import WagtailAPIRouter
-from wagtail.api.v2 import endpoints
+
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
+from wagtail.images.api.v2.endpoints import ImagesAPIEndpoint
+from wagtail.documents.api.v2.endpoints import DocumentsAPIEndpoint
+
 from rest_framework.permissions import AllowAny
 
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
 
 
-class PagesAPIEndpoint(endpoints.PagesAPIEndpoint):
+class PagesView(PagesAPIEndpoint):
     permission_classes = (AllowAny,)
 
 
-class ImagesAPIEndpoint(endpoints.ImagesAPIEndpoint):
+class ImagesView(ImagesAPIEndpoint):
     permission_classes = (AllowAny,)
 
 
-class DocumentsAPIEndpoint(endpoints.DocumentsAPIEndpoint):
+class DocumentsView(DocumentsAPIEndpoint):
     permission_classes = (AllowAny,)
 
 
@@ -22,6 +26,6 @@ class DocumentsAPIEndpoint(endpoints.DocumentsAPIEndpoint):
 # The first parameter is the name of the endpoint (eg. pages, images). This
 # is used in the URL of the endpoint
 # The second parameter is the endpoint class that handles the requests
-api_router.register_endpoint('pages', PagesAPIEndpoint)
-api_router.register_endpoint('images', ImagesAPIEndpoint)
-api_router.register_endpoint('documents', DocumentsAPIEndpoint)
+api_router.register_endpoint('pages', PagesView)
+api_router.register_endpoint('images', ImagesView)
+api_router.register_endpoint('documents', DocumentsView)
