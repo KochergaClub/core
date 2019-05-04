d27254bce3a76574241eb3aa8f8aafbf555ed862
Vyacheslav Matyukhin <me@berekuk.ru>
2019-05-04 20:24:19 +0300

    lint fix

--- kocherga/watchmen_routine/wagtail_hooks.py
+++ kocherga/watchmen_routine/wagtail_hooks.py
@@ -7,11 +7,6 @@ class TaskAdmin(ModelAdmin):
     model = models.Task
     menu_label = 'Админские таски'
     menu_icon = 'list-ul'
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
-    # list_display = ('title', 'author')
-    # list_filter = ('author',)
-    # search_fields = ('title', 'author')
 
 
 class RewardImageAdmin(ModelAdmin):
@@ -24,5 +19,6 @@ class WatchmanGroup(ModelAdminGroup):
     menu_icon = 'group'
     menu_label = 'Админы'
     items = (TaskAdmin, RewardImageAdmin, )
+
 
 modeladmin_register(WatchmanGroup)
