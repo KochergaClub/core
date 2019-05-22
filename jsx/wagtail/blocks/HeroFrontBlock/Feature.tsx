9af449cd94b0cc2e5a905393e3319c8ef90cddcc
Vyacheslav Matyukhin <me@berekuk.ru>
2019-05-22 20:50:41 +0300

    links in hero block

--- jsx/wagtail/blocks/HeroFrontBlock/Feature.tsx
+++ jsx/wagtail/blocks/HeroFrontBlock/Feature.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { fonts, colors } from '@kocherga/frontkit';
 
@@ -10,20 +10,47 @@ const Container = styled.div`
   margin-bottom: 16px;
 `;
 
+const hiddenLink = css`
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
 const Title = styled.div`
   font-size: ${fonts.sizes.XL};
   line-height: 1.2;
+
+  ${hiddenLink};
 `;
 
 const Items = styled.div`
   font-size: ${fonts.sizes.S};
   color: ${colors.grey[800]};
+
+  ${hiddenLink};
 `;
 
 const Feature = (props: FeatureType) => (
   <Container>
-    <Title>{props.title}</Title>
-    <Items>{props.items.map(item => item.text).join(' · ')}</Items>
+    <Title>
+      {props.link ? <a href={props.link}>{props.title}</a> : props.title}
+    </Title>
+    <Items>
+      {props.items
+        .map(
+          item =>
+            item.link ? (
+              <a href={item.link}>{item.text}</a>
+            ) : (
+              <span>{item.text}</span>
+            )
+        )
+        .reduce(
+          (accu, elem) => (accu.length ? [...accu, ' · ', elem] : [elem]),
+          []
+        )}
+    </Items>
   </Container>
 );
 
