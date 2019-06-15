b4be0ea6f09b97cbe1293edea5baf156d880a33a
Vyacheslav Matyukhin <me@berekuk.ru>
2019-06-15 17:30:15 +0300

    update (skipped/manual) fb test

--- tests/fb_test.py
+++ tests/fb_test.py
@@ -6,9 +6,10 @@ from kocherga.fb.models import Auth
 
 @pytest.mark.skip(reason="Manual test with short-living token")
 def test_long_term():
-    TOKEN = 'EAAX9MaZBcgKIBAGZCKieMOzJBhoLbti7ZCjZAJZBcEZCgDoCa1JPZAkWg44en3yuibQPk0q7zH7rZANZCT3GwItqS6KJOeyTwWFoxZALJx9C4icZBYcfM45IrP9UW3ABF6l6E5RcNsoi1nQbJWylbWZBlMZBBRIorLh9ez3UaQrxBWIN8rZAVyMENAzXXKbMaB66lGUDYZD'
+    TOKEN = 'EAAFKU6CgnoIBAKFYBKuJ0jv3gI6QZBj0lg2nmQYxvX6Y0AXykIFvaZBroJabxHlZBbzXitCud39BQSUjP7FQRha4d5MrpoHtGNU9DhrTNk2D5X2oXjmOSlLWLOhBiYiKk9o4blONe4ZAsSflvLGk36i1IWpt09RlFNouMg6bdAZDZD'
     Auth.objects.set(TOKEN)
     auth = Auth.objects.get()
     auth.validate()
 
+    assert len(auth.access_token) >= 40
     assert auth.access_token != TOKEN
