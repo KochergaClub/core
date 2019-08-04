e9a2da0dceff237776a077558d408cfdf4df130e
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-04 19:48:43 +0300

    split watchmen reducer

--- jsx/watchmen/reducer.ts
+++ jsx/watchmen/reducer.ts
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 import { Schedule } from './types';
 
 import {
@@ -7,46 +9,47 @@ import {
   ActionTypes,
 } from './actions';
 
-interface State {
-  editing: boolean;
-  schedule?: Schedule;
-}
+type EditingState = boolean;
+type ScheduleState = Schedule;
 
-const initialState: State = {
-  editing: false,
-  schedule: undefined,
-};
-
-const reducer = (state: State = initialState, action: ActionTypes): State => {
+const editingReducer = (
+  state: EditingState = false,
+  action: ActionTypes
+): EditingState => {
   switch (action.type) {
-    case UPDATE_SHIFT:
-      const shift = action.payload.shift;
-      if (!state.schedule) {
-        return state;
-      }
-      const schedule = state.schedule;
-      return {
-        ...state,
-        schedule: {
-          ...schedule,
-          [shift.date]: schedule[shift.date].map(existingShift => {
-            return existingShift.shift === shift.shift ? shift : existingShift;
-          }),
-        },
-      };
-    case REPLACE_SCHEDULE:
-      return {
-        ...state,
-        schedule: action.payload.schedule,
-      };
     case SET_EDITING:
-      return {
-        ...state,
-        editing: action.payload,
-      };
+      return action.payload;
     default:
       return state;
   }
 };
+
+const scheduleReducer = (
+  schedule: ScheduleState = {},
+  action: ActionTypes
+): ScheduleState => {
+  switch (action.type) {
+    case UPDATE_SHIFT:
+      const shift = action.payload.shift;
+      if (!schedule) {
+        return schedule;
+      }
+      return {
+        ...schedule,
+        [shift.date]: schedule[shift.date].map(existingShift => {
+          return existingShift.shift === shift.shift ? shift : existingShift;
+        }),
+      };
+    case REPLACE_SCHEDULE:
+      return action.payload.schedule;
+    default:
+      return schedule;
+  }
+};
+
+const reducer = combineReducers({
+  editing: editingReducer,
+  schedule: scheduleReducer,
+});
 
 export default reducer;
