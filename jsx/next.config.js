ac81e19569ebc7683a4802e19b13e992234baabc
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-12 22:35:04 +0300

    react-ga

--- jsx/next.config.js
+++ jsx/next.config.js
@@ -3,4 +3,8 @@ module.exports = {
     config.resolve.alias['~'] = __dirname;
     return config;
   },
+  // Will be available on both server and client
+  publicRuntimeConfig: {
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
+  },
 };
