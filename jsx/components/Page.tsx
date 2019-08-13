535091ffe4f885e5e5d038a2377e3c0ce58de7d3
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-13 22:10:29 +0300

    fix fonts issue

--- jsx/components/Page.tsx
+++ jsx/components/Page.tsx
@@ -27,7 +27,11 @@ const Page = ({ title, team, children, noMenu, noFooter }: Props) => {
         <title>{title}</title>
         <link rel="stylesheet" href="/static/normalize.css" />
         <link rel="shortcut icon" href="/static/favicon.ico" />
-        <GlobalFonts />
+        {/* NOTE - <GlobalFonts /> doesn't work for some reason */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i&display=block&subset=cyrillic"
+          rel="stylesheet"
+        />
       </Head>
       {noMenu || <TildaMenu team={team || false} />}
       <ErrorBoundary>{children}</ErrorBoundary>
