96e64ba395a23c0408f5d9b1f2dad44f1dff96f2
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-13 21:27:17 +0300

    remove more unused code

--- jsx/core/routes.tsx
+++ jsx/core/routes.tsx
@@ -2,58 +2,10 @@ import React from 'react';
 
 import { Screen } from '~/common/types';
 
-import { Redirect } from 'react-router';
-
 export interface KochergaRoute {
   screen: Screen<{}>;
   path: string;
 }
-
-const route = (path: string, module: { default: Screen<{}> }) => ({
-  screen: module.default,
-  path,
-});
-
-const redirectRoute = (from: string, to: string) => ({
-  path: from,
-  screen: {
-    component: () => <Redirect to={to} />,
-  },
-});
-
-export const routes: KochergaRoute[] = [
-  // NOTE: these routes migrated to next.js
-  // route('/team/watchmen/', require('~/watchmen/index')),
-  // route('/team/kkm/', require('~/kkm/index')),
-  // route('/team/analytics/', require('~/analytics/index')),
-  // route('/team/zadarma/', require('~/zadarma/index')),
-  // route('/team/zadarma/pbx_call/:id/', require('~/zadarma/pbx_call')),
-  // route('/team/staff/', require('~/staff/index_page')),
-  // route('/team/staff/:id/', require('~/staff/member_page')),
-  // route('/team/cashier/', require('~/cashier/index')),
-  // route('/team/ratio/', require('~/ratio/pages/index')),
-  // route('/team/ratio/training/:name/', require('~/ratio/pages/training')),
-  // route('/my/', require('~/my/index')),
-  // route('/team/events/', require('~/events/index')),
-  // route(
-  //   '/team/ratio/training/:name/schedule/',
-  //   require('~/ratio/pages/schedule')
-  // ),
-  // route('/team/mastermind_dating/', require('~/mastermind_dating/pages/list')),
-  // route(
-  //   '/team/mastermind_dating/cohort/:id/',
-  //   require('~/mastermind_dating/pages/cohort')
-  // ),
-  // route('/', require('~/frontpage/index')),
-  // route('/projects/', require('~/projects/index')),
-  // route('/projects/:name/', require('~/projects/detail')),
-  // route('/event/:id/', require('~/events/pages/event')),
-  // route('/login/check-your-email', require('~/auth/check-your-email')),
-  // route('/login/magic-link', require('~/auth/magic-link')),
-  // route('/login', require('~/auth/login')),
-  redirectRoute('/team/', '/team/staff/'),
-  // TODO - code splitting
-];
 
 export const errorPages: { [k: number]: React.ComponentType } = {
   400: require('~/error-pages/500').default.component,
--- jsx/render/server/errorHandler.ts
+++ jsx/render/server/errorHandler.ts
@@ -1,51 +1,9 @@
 import express from 'express';
 
-//import { APIError } from '~/common/api';
-//import { selectUser } from '~/core/selectors';
-
-//import { setupFallbackStore } from './globalContext';
-
-const getSendError = (req: express.Request, res: express.Response) => {
-  return (code: number) => {
-    res.status(code);
-    res.send('ERROR');
-    //sendEntrypointHtml(
-    //  {
-    //    type: 'error',
-    //    code,
-    //  },
-    //  req,
-    //  res
-    //);
-  };
-};
-
-export const notFoundHandler: express.RequestHandler = (req, res, _) => {
-  getSendError(req, res)(404);
-};
-
 export const errorHandler: express.ErrorRequestHandler = (err, req, res, _) => {
   console.error(err);
 
-  //if (!req.reduxStore) {
-  //  setupFallbackStore(req);
-  //}
-
-  //if (!req.reduxStore) {
-  //  throw new Error(
-  //    "Internal code error - setupFallbackStore didn't setup the store"
-  //  );
-  //}
-
-  //const user = selectUser(req.reduxStore.getState());
-
-  //if (err instanceof APIError && err.status === 403 && !user.is_authenticated) {
-  //  const nextUrl = encodeURIComponent(req.url);
-  //  res.redirect(302, `/login?next=${nextUrl}`);
-  //  return;
-  //}
-
-  const sendError = getSendError(req, res);
+  // const sendError = getSendError(req, res);
 
   //if (
   //  err instanceof APIError &&
@@ -54,6 +12,4 @@ export const errorHandler: express.ErrorRequestHandler = (err, req, res, _) => {
   //  sendError(err.status);
   //  return;
   //}
-
-  sendError(500);
 };
