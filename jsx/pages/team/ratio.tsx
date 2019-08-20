47704922c12d180ed995ae73c57a73aaf931443a
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-20 23:04:59 +0300

    /team/ratio uses redux for trainings

--- jsx/pages/team/ratio.tsx
+++ jsx/pages/team/ratio.tsx
@@ -1,52 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Link from 'next/link';
 
 import { A } from '@kocherga/frontkit';
 
 import { NextPage } from '~/common/types';
 import Page from '~/components/Page';
-import { selectAPI } from '~/core/selectors';
 
-import { Training } from '~/ratio/types';
-import { getTrainings } from '~/ratio/api';
+import { loadTrainings } from '~/ratio/actions';
+import { selectTrainings } from '~/ratio/selectors';
 
-interface Props {
-  trainings: Training[];
-  children?: React.ReactNode;
-}
+const RatioIndexPage: NextPage = () => {
+  const trainings = useSelector(selectTrainings);
 
-const RatioIndexPage: NextPage<Props> = ({ trainings }) => (
-  <Page title="Ratio" team>
-    <Page.Title>Воркшопы и тренинги</Page.Title>
-    <Page.Main>
-      <small>
-        <A href="/admin/ratio/training/add/">Добавить тренинг</A>
-      </small>
-      <hr />
-      <ul>
-        {trainings.map(training => (
-          <li key={training.slug}>
-            <Link
-              href="/team/ratio/training/[slug]"
-              as={`/team/ratio/training/${training.slug}`}
-              passHref
-            >
-              <A>
-                {training.date} {training.name}
-              </A>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Page.Main>
-  </Page>
-);
+  return (
+    <Page title="Ratio" team>
+      <Page.Title>Воркшопы и тренинги</Page.Title>
+      <Page.Main>
+        <small>
+          <A href="/admin/ratio/training/add/">Добавить тренинг</A>
+        </small>
+        <hr />
+        <ul>
+          {trainings.map(training => (
+            <li key={training.slug}>
+              <Link
+                href="/team/ratio/training/[slug]"
+                as={`/team/ratio/training/${training.slug}`}
+                passHref
+              >
+                <A>
+                  {training.date} {training.name}
+                </A>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Page.Main>
+    </Page>
+  );
+};
 
-RatioIndexPage.getInitialProps = async ({ store: { getState } }) => {
-  const api = selectAPI(getState());
-
-  const trainings = await getTrainings(api);
-  return { trainings };
+RatioIndexPage.getInitialProps = async ({ store: { dispatch } }) => {
+  await dispatch(loadTrainings());
+  return {};
 };
 
 export default RatioIndexPage;
