5b4249735b4e9b6b7f27ae1c8e0c04f306be9726
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-29 02:10:38 +0300

    audit: extract GroupCard component

---
+++ jsx/audit/components/GroupCard.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { A, Column, Row } from '@kocherga/frontkit';
+
+import Card from '~/components/Card';
+import Badge from '~/components/Badge';
+import AsyncButton from '~/components/AsyncButton';
+
+import UserInfo from './UserInfo';
+
+import { Group, User, Permission } from '../types';
+
+import { selectPermissions } from '../selectors';
+import { removeUserFromGroup } from '../actions';
+
+interface Props {
+  group: Group;
+}
+
+const GroupCard: React.FC<Props> = ({ group }) => {
+  const dispatch = useDispatch();
+  const permissions = useSelector(selectPermissions);
+
+  // FIXME - move to selector
+  const permissionsObj: { [k: number]: Permission } = {};
+  permissions.forEach(permission => {
+    permissionsObj[permission.id] = permission;
+  });
+
+  const removeUserCb = useCallback(
+    async (user: User) => {
+      await dispatch(removeUserFromGroup(group, user));
+    },
+    [dispatch, group]
+  );
+
+  return (
+    <Card>
+      <Column>
+        <Row vCentered stretch>
+          <strong>{group.name}</strong>
+          <small>
+            <A href={`/wagtail/groups/${group.id}/`}>редактировать в wagtail</A>
+          </small>
+        </Row>
+        <Row>
+          {group.permissions.map(id => (
+            <Badge key={id}>{permissionsObj[id].name}</Badge>
+          ))}
+        </Row>
+        {group.user_set.map(user => (
+          <Row>
+            <UserInfo key={user.id} user={user} />
+            <AsyncButton small act={async () => removeUserCb(user)}>
+              удалить
+            </AsyncButton>
+          </Row>
+        ))}
+      </Column>
+    </Card>
+  );
+};
+
+export default GroupCard;
--- jsx/audit/components/GroupsList.tsx
+++ jsx/audit/components/GroupsList.tsx
@@ -1,64 +1,19 @@
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { A, Column, Row } from '@kocherga/frontkit';
+import { CardList } from '~/components/Card';
 
-import Card, { CardList } from '~/components/Card';
-import Badge from '~/components/Badge';
-import AsyncButton from '~/components/AsyncButton';
+import { selectGroups } from '../selectors';
 
-import { Permission } from '../types';
-import UserInfo from './UserInfo';
-
-import { Group, User } from '../types';
-import { selectGroups, selectPermissions } from '../selectors';
-import { removeUserFromGroup } from '../actions';
+import GroupCard from './GroupCard';
 
 const GroupsList: React.FC = () => {
   const groups = useSelector(selectGroups);
-  const permissions = useSelector(selectPermissions);
-  const dispatch = useDispatch();
-
-  const permissionsObj: { [k: number]: Permission } = {};
-  permissions.forEach(permission => {
-    permissionsObj[permission.id] = permission;
-  });
-
-  const removeUserCb = useCallback(
-    async (group: Group, user: User) => {
-      await dispatch(removeUserFromGroup(group, user));
-    },
-    [dispatch]
-  );
 
   return (
     <CardList>
       {groups.map(group => (
-        <Card key={group.id}>
-          <Column>
-            <Row vCentered stretch>
-              <strong>{group.name}</strong>
-              <small>
-                <A href={`/wagtail/groups/${group.id}/`}>
-                  редактировать в wagtail
-                </A>
-              </small>
-            </Row>
-            <Row>
-              {group.permissions.map(id => (
-                <Badge key={id}>{permissionsObj[id].name}</Badge>
-              ))}
-            </Row>
-            {group.user_set.map(user => (
-              <Row>
-                <UserInfo key={user.id} user={user} />
-                <AsyncButton small act={async () => removeUserCb(group, user)}>
-                  удалить
-                </AsyncButton>
-              </Row>
-            ))}
-          </Column>
-        </Card>
+        <GroupCard group={group} />
       ))}
     </CardList>
   );
