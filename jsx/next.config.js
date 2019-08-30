f6cf89e9d0b6d0bc9ca1bd713477cb6bca6d076c
Vyacheslav Matyukhin <me@berekuk.ru>
2019-08-31 02:59:57 +0300

    vkRetargetingId in nextjs public config

--- jsx/next.config.js
+++ jsx/next.config.js
@@ -8,5 +8,6 @@ module.exports = {
     googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
     facebookPixelId: process.env.FACEBOOK_PIXEL_ID,
     yandexMetrikaId: process.env.YANDEX_METRIKA_ID,
+    vkRetargetingId: process.env.VK_RETARGETING_ID,
   },
 };
