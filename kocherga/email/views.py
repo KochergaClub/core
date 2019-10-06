513a4f496e00ab1f4cf34134eb3a80aee5b5b03d
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-06 22:27:07 +0300

    fix python lint

--- kocherga/email/views.py
+++ kocherga/email/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework import viewsets, permissions, mixins, exceptions
+from rest_framework import viewsets, permissions, exceptions
 
 from . import serializers
 from . import models
