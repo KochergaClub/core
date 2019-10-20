c9e15e79aefd2b5c52b82c8d7ea9f35f43559499
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-20 17:20:24 +0300

    install minizinc in docker repo and fix shell wrapper

--- prepare_solver_linux.sh
+++ prepare_solver_linux.sh
@@ -4,7 +4,7 @@ if [[ -e minizinc ]]; then exit 0; fi
 mkdir minizinc 2>/dev/null
 
 pushd minizinc
-wget 'https://github.com/MiniZinc/MiniZincIDE/releases/download/2.2.3/MiniZincIDE-2.2.3-bundle-linux-x86_64.tgz' -O mz.tgz
+wget -q 'https://github.com/MiniZinc/MiniZincIDE/releases/download/2.2.3/MiniZincIDE-2.2.3-bundle-linux-x86_64.tgz' -O mz.tgz
 tar -xzf mz.tgz
 rm mz.tgz
 mv MiniZinc* ide
@@ -13,10 +13,10 @@ mv MiniZinc* ide
 tee minizinc > /dev/null << "EOF"
 #!/bin/bash
 export PATH=`dirname $0`/ide/bin:$PATH
-export LD_LIBRARY_PATH=`dirname $0`/ide/bin:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=`dirname $0`/ide/lib:${LD_LIBRARY_PATH}
+export QT_PLUGIN_PATH=`dirname $0`/ide/plugins:${QT_PLUGIN_PATH}
 minizinc --output-mode json $@ | head -n -2
 EOF
 
 chmod +x minizinc
 popd
-
