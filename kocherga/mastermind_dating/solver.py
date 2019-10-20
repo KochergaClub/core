74f7260e3acd093c457b634d93599a4a3a3686b4
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-21 00:21:43 +0300

    fix parse_mode for solution broadcast

--- kocherga/mastermind_dating/solver.py
+++ kocherga/mastermind_dating/solver.py
@@ -33,8 +33,9 @@ async def broadcast_solution(cohort_id: int, bot: Bot):
         })
         tasks.append(asyncio.create_task(
             bot.send_message(
-                participant.get_chat_id(), message,
-                parse_mode="Markdown"
+                participant.get_chat_id(),
+                message,
+                # parse_mode="Markdown"
             )
         ))
 
