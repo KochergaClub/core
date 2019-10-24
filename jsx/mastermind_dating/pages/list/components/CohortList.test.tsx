a8d1b0b9d467946ce3699ff054d34293bd3d3c7d
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-24 03:32:13 +0300

    fix CohortList test

--- jsx/mastermind_dating/pages/list/components/CohortList.test.tsx
+++ jsx/mastermind_dating/pages/list/components/CohortList.test.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+import { configureStore } from '~/redux/store';
+import { configureAPI } from '~/core/actions';
+
 import CohortCollection from './CohortCollection';
 
 it('empty list renders without crashing', () => {
+  const store = configureStore();
+  store.dispatch(configureAPI({ csrfToken: 'fake' }));
+
   const div = document.createElement('div');
-  ReactDOM.render(<CohortCollection cohorts={[]} />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <CohortCollection cohorts={[]} />
+    </Provider>,
+    div
+  );
 });
