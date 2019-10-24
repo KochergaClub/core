8f6187dbbc20939eec89fe799242423eab16550f
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-24 03:31:51 +0300

    fix py lint

--- kocherga/events/views/events.py
+++ kocherga/events/views/events.py
@@ -15,7 +15,7 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework import status, generics, viewsets, filters, pagination
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, AllowAny
 
 from kocherga.error import PublicError
