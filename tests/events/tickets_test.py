e581b32e1f89f104222b9331ec72eaf80ced0ff1
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-28 21:21:25 +0300

    fix tickets test

--- tests/events/tickets_test.py
+++ tests/events/tickets_test.py
@@ -9,7 +9,6 @@ from django.conf import settings
 from django.core import mail
 from django.contrib.auth import get_user_model
 
-import kocherga.mailchimp
 from kocherga.dateutils import TZ
 from kocherga.events.models import Event, Ticket
 
@@ -21,7 +20,7 @@ def user1():
 
 @pytest.fixture
 def user2():
-    return get_user_model().objects.create_user('kocherga-test@berekuk.ru')
+    return get_user_model().objects.create_user('kocherga-test2@berekuk.ru')
 
 
 @pytest.fixture
@@ -66,7 +65,7 @@ def test_send_reminders(user1, user2, tomorrow_event, future_event):
     Ticket.objects.send_reminders()
 
     assert len(mail.outbox) == 5  # 3 registrations and 2 reminders
-    assert mail.outbox[2].subject == 'Регистрация на событие'
+    assert mail.outbox[2].subject.startswith('Регистрация на событие')
     assert mail.outbox[3].subject == f'Напоминание о событии: {tomorrow_event.title}'
     assert mail.outbox[4].subject == f'Напоминание о событии: {tomorrow_event.title}'
 
