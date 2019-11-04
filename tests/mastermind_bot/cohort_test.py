ba2ac1ac827ebaebf117a417728c20ce826d056e
Vyacheslav Matyukhin <me@berekuk.ru>
2019-11-04 20:48:16 +0300

    factory in mastermind tests

--- tests/mastermind_bot/cohort_test.py
+++ tests/mastermind_bot/cohort_test.py
@@ -4,11 +4,36 @@ pytestmark = [
     pytest.mark.google,
 ]
 
+from datetime import datetime
+import factory
+
 from django.contrib.auth import get_user_model
+
+from kocherga.dateutils import TZ
 from kocherga.mastermind_dating.models import Cohort, Participant
 from kocherga.timepad.models import Event as TimepadEvent, Order as TimepadOrder
 
 KchUser = get_user_model()
+
+
+class TimepadEventFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TimepadEvent
+
+    name = factory.Faker('sentence', nb_words=4)
+    id = factory.Sequence(lambda n: n)
+
+
+class TimepadOrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TimepadOrder
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    status = 'ok'
+    created_at = factory.LazyFunction(lambda: datetime.now(TZ))
+    subscribed_to_newsletter = False
+    id = factory.Sequence(lambda n: n)
 
 
 @pytest.fixture
@@ -17,33 +42,18 @@ def event_with_timepad(event, common_team):
     ann.link = 'https://kocherga-dev.timepad.ru/event/111'
     ann.save()
 
-    timepad_event = TimepadEvent.objects.create(
-        id=111,
-        name='abc',
-    )
+    timepad_event = TimepadEventFactory.create(id=111)
 
-    TimepadOrder.objects.create(
-        id=1,
+    TimepadOrderFactory.create(
         event=timepad_event,
-        first_name='Eliezer',
-        last_name='Yudkowsky',
-        status='ok',
         user=KchUser.objects.get(email='yudkowsky@example.com'),
     )
-    TimepadOrder.objects.create(
-        id=2,
+    TimepadOrderFactory.create(
         event=timepad_event,
-        first_name='Test',
-        last_name='User',
-        status='ok',
         user=KchUser.objects.get(email='test@kocherga-club.ru'),
     )
 
     return event
-
-
-def test_event_with_timepad(event_with_timepad):
-    assert event_with_timepad.timepad_event().id == 111
 
 
 def test_populate_from_event(event_with_timepad):
