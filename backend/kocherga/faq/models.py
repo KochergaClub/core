89a37bae5e2ef356972b1311843a5978ff5af39f
Vyacheslav Matyukhin <me@berekuk.ru>
2019-12-05 19:49:48 +0300

    faq: prev/next page navigation

--- backend/kocherga/faq/models.py
+++ backend/kocherga/faq/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from rest_framework import fields
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Orderable, Page
 from wagtail.api import APIField
+from wagtail.api.v2.serializers import PageSerializer, get_serializer_class
 from modelcluster.fields import ParentalKey
 
 from condensedinlinepanel.edit_handlers import CondensedInlinePanel
 
 from kocherga.wagtail.mixins import HeadlessPreviewMixin
+
+
+class NavPageField(fields.Field):
+    def to_representation(self, value):
+        # adapted from wagtail.api.v2.serializers.PageParentField
+        serializer_class = get_serializer_class(
+            value.__class__,
+            ['id', 'type', 'detail_url', 'html_url', 'title'],
+            meta_fields=['type', 'detail_url', 'html_url'],
+            base=PageSerializer
+        )
+        serializer = serializer_class(context=self.context)
+        return serializer.to_representation(value)
 
 
 class FAQPage(HeadlessPreviewMixin, Page):
@@ -17,6 +32,14 @@ class FAQPage(HeadlessPreviewMixin, Page):
     parent_page_types = ['pages.FrontPage', 'faq.FAQPage']
     subpage_types = ['faq.FAQPage']
 
+    @property
+    def prev_page(self):
+        return self.get_prev_siblings().type(self.__class__).live().first()
+
+    @property
+    def next_page(self):
+        return self.get_next_siblings().type(self.__class__).live().first()
+
     content_panels = Page.content_panels + [
         FieldPanel('summary'),
         CondensedInlinePanel('entries', label="Вопросы и ответы"),
@@ -24,6 +47,8 @@ class FAQPage(HeadlessPreviewMixin, Page):
 
     api_fields = [
         APIField('summary'),
+        APIField('prev_page', serializer=NavPageField()),
+        APIField('next_page', serializer=NavPageField()),
     ]
     # We could add `entries` to `api_fields` here, but the naive implementation causes circular import issues.
     # So we'll rely on /api/faq/entry REST call for now.
