f49559f5d74b793da4f799695f06a80c8a072828
Vyacheslav Matyukhin <me@berekuk.ru>
2019-12-28 04:54:12 +0300

    really fix lint error

--- backend/kocherga/email/urls.py
+++ backend/kocherga/email/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 urlpatterns = [
-    path('my/email/subscription_status', views.MySubscriptionStatusView.as_view()), # deprecated
     path('my/email', views.MySubscriptionStatusView.as_view()),
     path('my/email/resubscribe', views.ResubscribeView.as_view()),
     path('my/email/unsubscribe', views.UnsubscribeView.as_view()),
