83425db8697ae12df881ed97ed371e9f75f35464
Vyacheslav Matyukhin <me@berekuk.ru>
2020-01-15 23:40:35 +0300

    common docker ports in .env

--- frontend/server/constants.ts
+++ frontend/server/constants.ts
@@ -1,6 +1,6 @@
 // Hosts are bound through docker-compose
 
-export const API_HOST = process.env.NO_DOCKER_DEV ? 'localhost:8001' : 'api';
+export const API_HOST = process.env.NO_DOCKER_DEV ? 'localhost:5302' : 'api';
 export const API_ASYNC_HOST = process.env.NO_DOCKER_DEV
   ? 'localhost:8001'
   : process.env.NODE_ENV
