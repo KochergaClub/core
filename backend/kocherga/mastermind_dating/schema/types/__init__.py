db57767e0d7005fa1affb0f1fba6b3f85780add8
Vyacheslav Matyukhin <me@berekuk.ru>
2020-01-16 04:17:39 +0300

    fix mastermind participant photo url in graphql

--- backend/kocherga/mastermind_dating/schema/types/__init__.py
+++ backend/kocherga/mastermind_dating/schema/types/__init__.py
@@ -6,7 +6,14 @@ MastermindDatingCohort = DjangoObjectType('MastermindDatingCohort', models.Cohor
 MastermindDatingCohort.related_field('participants')
 MastermindDatingCohort.related_field('groups')
 
+
 MastermindDatingParticipant = DjangoObjectType('MastermindDatingParticipant', models.Participant)
+
+
+@MastermindDatingParticipant.field('photo')
+def resolve_MastermindDatingParticipant_photo(obj, info):
+    return obj.photo.url if obj.photo else None
+
 
 MastermindDatingGroup = DjangoObjectType('MastermindDatingGroup', models.Group)
 MastermindDatingGroup.related_field('participants')
