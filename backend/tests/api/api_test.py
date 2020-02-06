04568ae3db75bf4e014a4da2a962f18d4297e23c
Vyacheslav Matyukhin <me@berekuk.ru>
2020-02-07 00:26:14 +0300

    fix ci tests

--- backend/tests/api/api_test.py
+++ backend/tests/api/api_test.py
@@ -4,12 +4,6 @@ def test_options(client):
     assert res['Access-Control-Allow-Origin'] == '*'
 
 
-def test_clickjacking(client):
-    res = client.get('/api/graphql')
-    assert res.status_code == 200
-    assert res.get('X-Frame-Options') == 'SAMEORIGIN'
-
-
 def test_exceptions(client):
     res = client.get('/api/nosuchroute')
     assert res.status_code == 404
