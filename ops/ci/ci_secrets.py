f3591c55a0a4610f6e7902bd06276f8d28d1fcf4
Vyacheslav Matyukhin <me@berekuk.ru>
2020-02-06 22:04:11 +0300

    fix docker run options order; commit ci configs

---
+++ ops/ci/ci_secrets.py
@@ -0,0 +1,70 @@
+SECRET_KEY = "CI secret key"
+
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+
+KOCHERGA_JWT_SECRET_KEY = "CI JWT secret"
+
+# kocherga-core.ses-smtp-user
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = "/ByRRvQCLA8NlTl46haad"
+
+KOCHERGA_MAILCHIMP_UID_SALT = "whatever"
+
+CAFE_MANAGER_CREDENTIALS = {"login": "admin", "password": "admin"}
+
+CAFE_MANAGER_SERVER = "https://demo.cafe-manager.ru"
+
+KOCHERGA_FB_ANNOUNCER_PASSWORD = ""
+
+KOCHERGA_FB_MARKETING = {
+    "access_token": "",
+    "account_id": "",
+    "audiences": {
+        "ratio_tickets": "",
+    },
+}
+KOCHERGA_GOOGLE_CREDENTIALS = {
+    "type": "service_account",
+    "project_id": "",
+    "private_key_id": "",
+    "private_key": "",
+    "client_email": "",
+    "client_id": "",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "",
+}
+
+KOCHERGA_MAILCHIMP_API_KEY = "-us18"
+
+KOCHERGA_MONEY_OFD_YA_TOKEN = ""
+
+KOCHERGA_RUCAPTCHA_KEY = ""
+
+KOCHERGA_SLACK_BOT_TOKEN = ""
+KOCHERGA_SLACK_SIGNING_SECRET = ""
+
+KOCHERGA_TELEGRAM_TOKEN = ""
+
+KOCHERGA_TIMEPAD_TOKEN = ""
+
+KOCHERGA_MONEY_TOCHKA_CLIENT = {
+    "client_id": "sandbox",
+    "client_secret": "sandbox_secret",
+}
+
+KOCHERGA_VK_API_KEY = ""
+
+KOCHERGA_WIKI_PASSWORD = ""
+
+YANDEX_KASSA = {
+    "shopId": 1,
+    "secret": "",
+}
+
+MASTERMIND_BOT_CONFIG = {
+    "token": "",
+    "bot_link": "",
+}
