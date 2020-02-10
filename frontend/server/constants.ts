79253dcb2aae36bc825cd3938fe0ecb5ef0a2e27
Vyacheslav Matyukhin <me@berekuk.ru>
2020-02-10 20:39:23 +0300

    DJANGO_HOST env for frontend; fix/improve backups

--- frontend/server/constants.ts
+++ frontend/server/constants.ts
@@ -1,3 +1,3 @@
 // Hosts are bound through k8s
 
-export const API_HOST = 'kocherga-backend';
+export const API_HOST = process.env.DJANGO_HOST || 'kocherga-django'; // default value is probably incorrect
--- ops/images/mysql-backup/backup.sh
+++ ops/images/mysql-backup/backup.sh
@@ -5,14 +5,35 @@ if [ -z "$S3_BACKUP_BUCKET" ]; then
     exit 1
 fi
 
+if [ -z "$S3_PATH" ]; then
+    echo "S3_PATH is not set"
+    exit 1
+fi
+
+if [ -z "$DB_HOST" ]; then
+    echo "DB_HOST is not set"
+    exit 1
+fi
+
+if [ -z "$DB_USER" ]; then
+    echo "DB_USER is not set"
+    exit 1
+fi
+
 if [ -z "$DB_PASSWORD" ]; then
     echo "DB_PASSWORD is not set"
     exit 1
 fi
 
-TMP_FILE=/tmp/all-databases.gz
+if [ -z "$DB_NAME" ]; then
+    echo "DB_NAME is not set"
+    exit 1
+fi
+
+
+TMP_FILE=/tmp/dump.gz
 
 set -e
-mysqldump --host=kocherga-mysql --user=kocherga --password=$DB_PASSWORD kocherga | gzip >$TMP_FILE
+mysqldump --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD $DB_NAME | gzip >$TMP_FILE
 
-aws s3 cp $TMP_FILE s3://$S3_BACKUP_BUCKET/kocherga-mysql/$(date -Idate).gz
+aws s3 cp $TMP_FILE s3://$S3_BACKUP_BUCKET/$S3_PATH/$(date -Idate).gz
