005420ebe5f8367df7dc33ea88aa79c75566962e
Vyacheslav Matyukhin <me@berekuk.ru>
2020-03-24 23:21:00 +0300

    remove unused faq DRF code

--- backend/kocherga/faq/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-
-
-class EntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Entry
-        fields = ('id', 'question', 'answer')
--- backend/kocherga/faq/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from . import views
-
-router = routers.SimpleRouter(trailing_slash=False)
-router.register(r'faq/entry', views.EntryViewSet, basename='faq-entries')
-
-urlpatterns = router.urls
