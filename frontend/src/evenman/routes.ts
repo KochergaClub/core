293f2b2244aba491d44dc06082e6991a05369a24
Vyacheslav Matyukhin <me@berekuk.ru>
2020-04-15 23:50:58 +0300

    evenman: delete redirects to root

--- frontend/src/evenman/routes.ts
+++ frontend/src/evenman/routes.ts
@@ -3,6 +3,11 @@ interface Route {
   as: string;
 }
 
+export const rootRoute = (): Route => ({
+  href: '/team/evenman',
+  as: '/team/evenman',
+});
+
 export const eventRoute = (id: string): Route => ({
   href: '/team/evenman/event/[id]',
   as: `/team/evenman/event/${id}`,
