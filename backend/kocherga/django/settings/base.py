fb01faeef4c2ba14873a3496fe75e203ac68157f
Vyacheslav Matyukhin <me@berekuk.ru>
2020-04-26 04:26:00 +0300

    upgrade python deps to latest versions; remove unused whitenoise

--- backend/kocherga/django/settings/base.py
+++ backend/kocherga/django/settings/base.py
@@ -99,7 +99,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'kocherga.django.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
