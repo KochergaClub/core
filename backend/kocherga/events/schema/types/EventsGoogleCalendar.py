a4ee8a12ee4273fc77fb219de03acdf95451feae
Vyacheslav Matyukhin <me@berekuk.ru>
2020-06-05 04:03:12 +0300

    fix EventsGoogleCalendar graphql resolvers

--- backend/kocherga/events/schema/types/EventsGoogleCalendar.py
+++ backend/kocherga/events/schema/types/EventsGoogleCalendar.py
@@ -3,3 +3,5 @@ from kocherga.graphql.types import DjangoObjectType
 from ... import models
 
 EventsGoogleCalendar = DjangoObjectType('EventsGoogleCalendar', models.GoogleCalendar)
+
+EventsGoogleCalendar.simple_property_field('url')
