2dcadd4b8c88fcb9910df1caaf4962a219df5b50
Vyacheslav Matyukhin <me@berekuk.ru>
2020-07-10 04:51:14 +0300

    fix qs handling in ssr redirect in FallbackPage

--- frontend/src/cms/components/FallbackPage.tsx
+++ frontend/src/cms/components/FallbackPage.tsx
@@ -1,12 +1,25 @@
 import { Spinner } from '~/components';
 import { useEffect } from 'react';
 
+import { parseQueryString, buildQueryString } from '~/common/utils';
+
 const FallbackPage: React.FC = () => {
   useEffect(() => {
-    if (window.location.href.includes('?ssr=1')) {
+    if (
+      window.location.href.includes('?ssr=1') ||
+      window.location.href.includes('&ssr=1')
+    ) {
       return; // huh? shouldn't happen since ssr=1 is processed by our custom server, but let's avoid infinite loop
     }
-    window.location.href = `${window.location.href}?ssr=1`;
+
+    window.location.replace(
+      window.location.pathname +
+        '?' +
+        buildQueryString({
+          ...parseQueryString(window.location.search),
+          ssr: '1',
+        })
+    );
   }, []);
   return <Spinner size="block" />;
 };
