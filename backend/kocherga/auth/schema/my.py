e826b81b1ed25faa95731059d714c0a92cba3e26
Vyacheslav Matyukhin <me@berekuk.ru>
2020-07-14 23:07:30 +0300

    fix tests

--- backend/kocherga/auth/schema/my.py
+++ backend/kocherga/auth/schema/my.py
@@ -1,22 +1,14 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-from kocherga.graphql import g
-from kocherga.graphql.helpers import Collection
+from kocherga.graphql import g, helpers
 from . import types
 
 
-c = Collection()
+c = helpers.Collection()
 
 
 # user: AuthCurrentUser!
 @c.field
 def user(helper):
     def resolve(_, info):
-        logger.info(info.operation)
-        logger.info(info.operation.name)
-        logger.info(info.operation.name.value)
         return info.context.user
 
     return g.Field(g.NN(types.AuthCurrentUser), resolve=resolve)
