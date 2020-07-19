95180f95d9967bacadf4edcc42075833e929b2e8
Vyacheslav Matyukhin <me@berekuk.ru>
2020-07-19 22:19:18 +0300

    fix AuthUser.staff_member field

--- backend/kocherga/auth/schema/types/AuthUser.py
+++ backend/kocherga/auth/schema/types/AuthUser.py
@@ -1,22 +1,11 @@
 from kocherga.graphql import g
 from kocherga.staff.schema.types import StaffMember
 
-# type AuthUser {
-#   id: ID!
-#   email: String!
-#   staff_member: StaffMember
-# }
-
-
-def resolve_staff_member(obj, info):
-    return obj.staff_member  # TODO - dataloader
-
-
 AuthUser = g.ObjectType(
     'AuthUser',
     fields={
         'id': g.Field(g.NN(g.ID)),
         'email': g.Field(g.NN(g.String)),
-        'staff_member': g.Field(StaffMember, resolve=resolve_staff_member),
+        'staff_member': g.Field(StaffMember),
     },
 )
