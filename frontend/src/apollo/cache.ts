af407eddaff9cd0669b1a15854d6e285a20fe405
Vyacheslav Matyukhin <me@berekuk.ru>
2020-07-22 03:15:20 +0300

    apollo v3: dataIdFromObject -> typePolicies

--- frontend/src/apollo/cache.ts
+++ frontend/src/apollo/cache.ts
@@ -1,26 +1,20 @@
-import { InMemoryCache, defaultDataIdFromObject } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client';
 
-import { WatchmenShift } from './types.generated';
 import introspectionResult from './introspection-result.generated';
 
 class KochergaApolloCache extends InMemoryCache {
   constructor() {
     super({
-      dataIdFromObject: object => {
-        switch (object.__typename) {
-          case 'WatchmenShift':
-            const watchmenShift = object as WatchmenShift;
-            return (
-              'WatchmenShift:' + watchmenShift.date + '/' + watchmenShift.shift
-            );
-          case 'AuthCurrentUser':
-            // current user is a singleton but doesn't contain a real ID since we don't want to leak integer ids from the database
-            return 'AuthCurrentUser:me';
-          case 'EventsPublicEvent':
-            return (object as any).event_id; // TODO - migrate to `id` field which is now returned by backend
-          default:
-            return defaultDataIdFromObject(object);
-        }
+      typePolicies: {
+        WatchmenShift: {
+          keyFields: ['date', 'shift'],
+        },
+        AuthCurrentUser: {
+          keyFields: ['email'],
+        },
+        EventsPublicEvent: {
+          keyFields: ['event_id'], // TODO - migrate to `id` field which is now returned by backend
+        },
       },
       possibleTypes: introspectionResult.possibleTypes,
     });
