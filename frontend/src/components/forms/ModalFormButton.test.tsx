c81779e40ca9974178b420f0ae063180bb885eee
Vyacheslav Matyukhin <me@berekuk.ru>
2020-08-30 05:00:14 +0300

    minimal test for ModalFormButton

---
+++ frontend/src/components/forms/ModalFormButton.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, waitFor } from '@testing-library/react';
+
+import { render } from '~/test/utils';
+
+import ModalFormButton from './ModalFormButton';
+import { FormShape } from './types';
+
+it('renders without crashing', async () => {
+  const shape: FormShape = [
+    {
+      name: 'foo',
+      type: 'string',
+      optional: true,
+    },
+  ];
+  let posted: any;
+  const post = async (v: any) => {
+    posted = v;
+  };
+  const { getByText, queryByText } = render(
+    <ModalFormButton
+      shape={shape}
+      buttonName="Кнопка"
+      modalButtonName="Отправить"
+      modalTitle="Форма"
+      post={post}
+    />
+  );
+
+  expect(queryByText('Отправить')).toBeNull();
+
+  fireEvent.click(getByText('Кнопка'));
+  await waitFor(() => getByText('Отправить'));
+  expect(getByText('Отправить')).not.toHaveAttribute('disabled');
+
+  fireEvent.click(getByText('Отправить'));
+  await waitFor(() => !getByText('Отправить'));
+
+  expect(posted).toEqual({ foo: '' });
+});
