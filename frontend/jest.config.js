80e3d0c40d0badd97c5b4785535bd00327c215ea
Vyacheslav Matyukhin <me@berekuk.ru>
2020-09-21 23:45:56 +0300

    fix jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   moduleNameMapper: {
-    '^~/(.*)$': '<rootDir>/src/$1',
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
+    '^~/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
