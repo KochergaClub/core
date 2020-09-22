10fe8f2d8d4c791b3e0ca0303a4d570810c39ee8
Vyacheslav Matyukhin <me@berekuk.ru>
2020-09-22 04:19:27 +0300

    prometheus histogram for all graphql requests

--- backend/kocherga/graphql/views.py
+++ backend/kocherga/graphql/views.py
@@ -2,6 +2,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+from timeit import default_timer
+from prometheus_client import Histogram
+
 from django.http import HttpRequest
 from django.conf import settings
 
@@ -11,13 +14,21 @@ from ariadne.contrib.tracing.apollotracing import ApolloTracingExtensionSync
 
 from kocherga.graphql.schema import schema
 
+duration_histogram = Histogram(
+    'graphql_request', 'Duration of GraphQL requests', ['operation']
+)
+
 
 class WrappedGraphQLView(GraphQLView):
     def execute_query(self, request: HttpRequest, data: dict) -> GraphQLResult:
-        logger.info(
-            'GraphQL: ' + (data.get('operationName', None) or 'unknown operation')
-        )
-        return super().execute_query(request, data)
+        start = default_timer()
+        result = super().execute_query(request, data)
+        duration = default_timer() - start
+
+        operation = data.get('operationName', None) or 'unknown operation'
+        duration_histogram.labels(operation=operation).observe(duration)
+        logger.info(f'GraphQL: {operation} [{duration:.3f}]')
+        return result
 
 
 kocherga_graphql_view = WrappedGraphQLView.as_view(
