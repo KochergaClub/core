07c614dc08ca1602596d05ad77f464bd3a50d793
Vyacheslav Matyukhin <me@berekuk.ru>
2020-09-25 19:50:57 +0300

    fix non-empty chats check on project pages

--- frontend/src/projects/wagtail/ProjectPage/index.tsx
+++ frontend/src/projects/wagtail/ProjectPage/index.tsx
@@ -22,7 +22,7 @@ const ProjectPage: NextWagtailPage<ProjectPageFragment> = ({
       <ProjectHeroBlock project={project} />
       <RichTextBlock html={project.body} />
       <UpcomingEventsBlock project={project} />
-      {project.telegram_chats ? (
+      {project.telegram_chats.length ? (
         <div>
           <TL03
             title={`Чат${project.telegram_chats.length > 1 ? 'ы' : ''} проекта`}
