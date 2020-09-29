8882b823fc7870f1020af4da2ed5df58bf3589fa
Vyacheslav Matyukhin <me@berekuk.ru>
2020-09-29 22:18:27 +0300

    CORS fix

--- frontend/server/nextjsEntrypoint.ts
+++ frontend/server/nextjsEntrypoint.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
-import { parse } from 'url';
-
 import Server from 'next/dist/next-server/server/next-server';
+import { parse } from 'url';
 
 export default (nextApp: Server) => async (
   req: express.Request,
   res: express.Response
 ) => {
   const parsedUrl = parse(req.url, true);
+
+  if (req.url.startsWith('/_next/')) {
+    // necessary because we serve CSS from CDN, and sometimes Next.JS injects CSS with JS code.
+    // So without this line we'd get "Not allowed to access cross-origin stylesheet" JS error on pages which load CSS dynamically.
+    res.header('Access-Control-Allow-Origin', '*');
+  }
 
   if (parsedUrl.query.ssr === '1') {
     return nextApp.getRequestHandler()(
