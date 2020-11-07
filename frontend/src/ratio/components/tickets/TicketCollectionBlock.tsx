a36b9fc1b6e4c56084b16c13f326e2b1cb86c293
Vyacheslav Matyukhin <me@berekuk.ru>
2020-11-07 16:24:43 +0300

    animated tickets list

--- frontend/src/ratio/components/tickets/TicketCollectionBlock.tsx
+++ frontend/src/ratio/components/tickets/TicketCollectionBlock.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useCallback, useState } from 'react';
 import { MdRefresh } from 'react-icons/md';
 
@@ -40,51 +41,61 @@ const TicketCollectionBlock: React.FC = () => {
 
   return (
     <PaddedBlock width="wide">
-      <ApolloQueryResults {...queryResults} size="block">
-        {({ data: { tickets } }) => (
-          <HeadlessConnection
-            connection={tickets}
-            fetchPage={queryResults.refetch}
-          >
-            {({ items, next, previous }) => (
-              <Column stretch gutter={16}>
-                <h2>
-                  <Row gutter={8}>
-                    <div>Билеты</div>
-                    <AsyncButton act={queryResults.refetch}>
-                      <Row vCentered>
-                        <MdRefresh />
-                        <span>Обновить</span>
-                      </Row>
-                    </AsyncButton>
-                  </Row>
-                </h2>
-                <RadioButtonGroup selected={filter} select={asyncSetFilter}>
-                  <RadioButtonGroup.Button name="all">
-                    Все
-                  </RadioButtonGroup.Button>
-                  <RadioButtonGroup.Button name="unfiscalized">
-                    С непробитыми чеками
-                  </RadioButtonGroup.Button>
-                  <RadioButtonGroup.Button name="missing-payments">
-                    С недостающими платежами
-                  </RadioButtonGroup.Button>
-                </RadioButtonGroup>
-                <Column gutter={16} stretch>
-                  {items.map((ticket) => (
-                    <TicketCard key={ticket.id} ticket={ticket} />
-                  ))}
-                </Column>
-                <Pager
-                  next={next}
-                  previous={previous}
-                  pageInfo={tickets.pageInfo}
-                />
-              </Column>
-            )}
-          </HeadlessConnection>
-        )}
-      </ApolloQueryResults>
+      <Column stretch gutter={16}>
+        <h2>
+          <Row gutter={8}>
+            <div>Билеты</div>
+            <AsyncButton act={queryResults.refetch}>
+              <Row vCentered>
+                <MdRefresh />
+                <span>Обновить</span>
+              </Row>
+            </AsyncButton>
+          </Row>
+        </h2>
+        <RadioButtonGroup selected={filter} select={asyncSetFilter}>
+          <RadioButtonGroup.Button name="all">Все</RadioButtonGroup.Button>
+          <RadioButtonGroup.Button name="unfiscalized">
+            С непробитыми чеками
+          </RadioButtonGroup.Button>
+          <RadioButtonGroup.Button name="missing-payments">
+            С недостающими платежами
+          </RadioButtonGroup.Button>
+        </RadioButtonGroup>
+        <ApolloQueryResults {...queryResults} size="block">
+          {({ data: { tickets } }) => (
+            <HeadlessConnection
+              connection={tickets}
+              fetchPage={queryResults.refetch}
+            >
+              {({ items, next, previous }) => (
+                <div>
+                  <Column gutter={16} stretch>
+                    <AnimatePresence initial={false}>
+                      {items.map((ticket) => (
+                        <motion.div
+                          initial={{ opacity: 0, scale: 0.5 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0.5 }}
+                          layout
+                          key={ticket.id}
+                        >
+                          <TicketCard ticket={ticket} />
+                        </motion.div>
+                      ))}
+                    </AnimatePresence>
+                  </Column>
+                  <Pager
+                    next={next}
+                    previous={previous}
+                    pageInfo={tickets.pageInfo}
+                  />
+                </div>
+              )}
+            </HeadlessConnection>
+          )}
+        </ApolloQueryResults>
+      </Column>
     </PaddedBlock>
   );
 };
