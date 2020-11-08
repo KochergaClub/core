9a2d35f695f9a70ce52ad57c8c110a5b198f1dd4
Vyacheslav Matyukhin <me@berekuk.ru>
2020-11-08 22:43:38 +0300

    sidebar on /team/kkm

--- frontend/src/kkm/pages/KkmPage.tsx
+++ frontend/src/kkm/pages/KkmPage.tsx
@@ -1,21 +1,71 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { NextApolloPage, withApollo, withStaff } from '~/apollo';
 import { PaddedBlock, Page } from '~/components';
+import { WithNavSidebar } from '~/frontkit';
 
 import MainForm from '../components/MainForm';
 import OfdFiscalDriveCollection from '../components/ofd/OfdFiscalDriveCollection';
 
+const tabs = [
+  { title: 'Пробить чек', name: 'custom-check' },
+  { title: 'Фискальные накопители', name: 'fiscal-drives' },
+];
+
 const KkmPage: NextApolloPage = () => {
+  const router = useRouter();
+
+  const renderTab = (name: string) => {
+    switch (name) {
+      case 'custom-check':
+        return (
+          <PaddedBlock>
+            <MainForm />
+          </PaddedBlock>
+        );
+      case 'fiscal-drives':
+        return <OfdFiscalDriveCollection />;
+      default:
+        return <div>Unknown route {name}</div>;
+    }
+  };
+  const detectTab = () => {
+    const route = router.pathname;
+    if (route === '/team/kkm') {
+      return 'custom-check';
+    } else if (route === '/team/kkm/fiscal-drives') {
+      return 'fiscal-drives';
+    } else {
+      return '';
+    }
+  };
+  const selected = detectTab();
+
+  const selectTab = (name: string) => {
+    const name2path: Record<string, string> = {
+      'custom-check': '/team/kkm',
+      'fiscal-drives': '/team/kkm/fiscal-drives',
+    };
+    const path = name2path[name];
+    if (path) {
+      router.push(path);
+    }
+  };
+
   return (
-    <Page title="Электронные чеки" menu="team">
-      <Page.Title>Электронные чеки</Page.Title>
-      <Page.Main>
-        <PaddedBlock>
-          <MainForm />
-        </PaddedBlock>
-        <OfdFiscalDriveCollection />
-      </Page.Main>
+    <Page title="Электронные чеки" chrome="fullscreen" menu="team">
+      <WithNavSidebar
+        tabs={tabs}
+        header={{
+          title: 'Касса и чеки',
+          tabName: 'custom-check',
+          href: '/team/kkm',
+        }}
+        selected={selected}
+        selectTab={selectTab}
+        renderTab={renderTab}
+      />
     </Page>
   );
 };
