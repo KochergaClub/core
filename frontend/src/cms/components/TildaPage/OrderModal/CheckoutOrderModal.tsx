f447b1c79c01de1fb874a89f5e54d0388247ee07
Vyacheslav Matyukhin <me@berekuk.ru>
2020-12-11 04:57:45 +0300

    script onerror when loading kassa widget

--- frontend/src/cms/components/TildaPage/OrderModal/CheckoutOrderModal.tsx
+++ frontend/src/cms/components/TildaPage/OrderModal/CheckoutOrderModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Modal } from '~/frontkit';
 import { confirmOrderRoute } from '~/ratio/routes';
@@ -6,21 +6,25 @@ import { confirmOrderRoute } from '~/ratio/routes';
 import { RatioOrder_CreatedFragment } from './queries.generated';
 
 // inspired by https://medium.com/better-programming/loading-third-party-scripts-dynamically-in-reactjs-458c41a7013d
-const loadKassaCheckoutUI = (cb: () => void) => {
-  const id = 'kassaCheckoutUI';
-  const existingScript = document.getElementById(id);
-  if (!existingScript) {
-    const script = document.createElement('script');
-    script.src = 'https://kassa.yandex.ru/checkout-ui/v2.js';
-    script.id = id;
-    document.body.appendChild(script);
-    script.onload = () => {
-      if (cb) {
-        cb();
-      }
-    };
-  }
-  if (existingScript && cb) cb();
+const loadKassaCheckoutUI = () => {
+  return new Promise((resolve, reject) => {
+    const id = 'kassaCheckoutUI';
+    const existingScript = document.getElementById(id);
+    if (existingScript) {
+      resolve();
+    } else {
+      const script = document.createElement('script');
+      script.src = 'https://kassa.yandex.ru/checkout-ui/v2.js';
+      script.id = id;
+      document.body.appendChild(script);
+      script.onload = () => {
+        resolve();
+      };
+      script.onerror = (e: Event | string) => {
+        reject(e);
+      };
+    }
+  });
 };
 
 interface CheckoutProps {
@@ -29,8 +33,15 @@ interface CheckoutProps {
 }
 
 const CheckoutOrderModal: React.FC<CheckoutProps> = ({ close, order }) => {
+  const [failed, setFailed] = useState('');
   useEffect(() => {
-    loadKassaCheckoutUI(() => {
+    (async () => {
+      try {
+        await loadKassaCheckoutUI();
+      } catch (e) {
+        setFailed('Не удалось загрузить виджет платёжной системы');
+        return;
+      }
       const return_url = `${
         process.env.NEXT_PUBLIC_KOCHERGA_WEBSITE
       }${confirmOrderRoute(order.id)}`;
@@ -44,13 +55,15 @@ const CheckoutOrderModal: React.FC<CheckoutProps> = ({ close, order }) => {
         },
       });
       checkout.render('kassa-checkout-form');
-    });
+    })();
   }, [order.id, order.confirmation_token]);
+
   return (
     <Modal>
       <Modal.Header close={close}>Регистрация</Modal.Header>
       <Modal.Body>
         <div id="kassa-checkout-form"></div>
+        {failed ? <div>{failed}</div> : null}
       </Modal.Body>
     </Modal>
   );
