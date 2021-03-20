a02f77050db7ec5ba82ac356db88807edbb25c18
Vyacheslav Matyukhin <me@berekuk.ru>
2021-03-20 16:11:52 +0300

    one more test fix

--- backend/tests/events/tickets_test.py
+++ backend/tests/events/tickets_test.py
@@ -25,9 +25,7 @@ class EventFactory(factory.django.DjangoModelFactory):
 
     title = factory.Faker('sentence', nb_words=4)
     event_type = 'public'
-    end = factory.LazyAttribute(
-        lambda o: o.start + timedelta(hours=1)
-    )
+    end = factory.LazyAttribute(lambda o: o.start + timedelta(hours=1))
 
 
 @pytest.fixture
@@ -59,15 +57,19 @@ def test_send_reminders(frozen_time, user1, user2, tomorrow_event, future_event)
     Ticket.objects.register(user=user2, event=tomorrow_event)
     Ticket.objects.register(user=user1, event=future_event)
     Ticket.objects.register(user=user2, event=future_event)
-    initial_reminders = 0 # should be 4, but emails were moved to async queue which is not processed in tests
+    initial_reminders = 0  # should be 4, but emails were moved to async queue which is not processed in tests
     assert len(mail.outbox) == initial_reminders
 
     Ticket.objects.send_reminders()
-    assert len(mail.outbox) == initial_reminders  # no reminders - events were just created
+    assert (
+        len(mail.outbox) == initial_reminders
+    )  # no reminders - events were just created
 
     frozen_time.tick(delta=timedelta(days=1))
     Ticket.objects.send_reminders()
-    assert len(mail.outbox) == initial_reminders  # no reminders - future events are 2 days ahead
+    assert (
+        len(mail.outbox) == initial_reminders
+    )  # no reminders - future events are 2 days ahead
 
     frozen_time.tick(delta=timedelta(days=1))
     Ticket.objects.send_reminders()
@@ -78,9 +80,17 @@ def test_send_reminders(frozen_time, user1, user2, tomorrow_event, future_event)
     Ticket.objects.send_reminders()
     assert len(mail.outbox) == initial_reminders + 2
 
-    assert mail.outbox[3].subject.startswith('Регистрация на событие')
-    assert mail.outbox[4].subject == f'Напоминание о событии: {future_event.title}'
-    assert mail.outbox[5].subject == f'Напоминание о событии: {future_event.title}'
+    assert mail.outbox[initial_reminders - 1].subject.startswith(
+        'Регистрация на событие'
+    )
+    assert (
+        mail.outbox[initial_reminders].subject
+        == f'Напоминание о событии: {future_event.title}'
+    )
+    assert (
+        mail.outbox[initial_reminders + 1].subject
+        == f'Напоминание о событии: {future_event.title}'
+    )
 
     Ticket.objects.send_reminders()
     assert len(mail.outbox) == initial_reminders + 2  # no additional reminders
