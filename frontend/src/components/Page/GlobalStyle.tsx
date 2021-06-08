33ea6b3d87b64c7bdcad94b28ff4ac8abd5967df
Vyacheslav Matyukhin <me@berekuk.ru>
2021-06-08 05:12:06 +0300

    fix font preloading

--- frontend/src/components/Page/GlobalStyle.tsx
+++ frontend/src/components/Page/GlobalStyle.tsx
@@ -4,31 +4,23 @@ import { staticUrl } from '~/common/utils';
 
 const GlobalStyle = () => (
   <Head>
-    <link rel="preload" href={staticUrl('fonts/intro.woff2')} />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-regular.woff2')}
-    />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-italic.woff2')}
-    />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-500.woff2')}
-    />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-500italic.woff2')}
-    />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-700.woff2')}
-    />
-    <link
-      rel="preload"
-      href={staticUrl('fonts/roboto-v27-latin_cyrillic-700italic.woff2')}
-    />
+    {[
+      'fonts/roboto-v27-latin_cyrillic-regular.woff2',
+      'fonts/roboto-v27-latin_cyrillic-italic.woff2',
+      'fonts/roboto-v27-latin_cyrillic-500.woff2',
+      'fonts/roboto-v27-latin_cyrillic-500italic.woff2',
+      'fonts/roboto-v27-latin_cyrillic-700.woff2',
+      'fonts/roboto-v27-latin_cyrillic-700italic.woff2',
+    ].map((url, i) => (
+      <link
+        key={i}
+        rel="preload"
+        href={staticUrl(url)}
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+    ))}
   </Head>
 );
 
