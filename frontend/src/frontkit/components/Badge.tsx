308609c3d23030c80083b882559f85290a7f2a7d
Vyacheslav Matyukhin <me@berekuk.ru>
2021-06-16 02:23:59 +0300

    fix tippy in Badge

--- frontend/src/frontkit/components/Badge.tsx
+++ frontend/src/frontkit/components/Badge.tsx
@@ -1,6 +1,7 @@
 import 'tippy.js/dist/tippy.css';
 
 import clsx from 'clsx';
+import { forwardRef } from 'react';
 
 import Tippy from '@tippyjs/react';
 
@@ -13,27 +14,29 @@ interface Props extends InnerProps {
   hint?: string;
 }
 
-export const InnerBadge: React.FC<InnerProps> = ({
-  type = 'default',
-  color,
-  children,
-}) => (
-  <div
-    className={clsx(
-      'inline-block rounded-xl px-2 py-0.5 text-xs whitespace-nowrap',
-      {
-        accent: 'bg-accent-500',
-        default: 'bg-primary-300',
-        good: 'bg-green-300',
-      }[type]
-    )}
-    style={{
-      ...(color && { backgroundColor: color }),
-    }}
-  >
-    {children}
-  </div>
-);
+const InnerBadge = forwardRef<
+  HTMLDivElement,
+  InnerProps & JSX.IntrinsicElements['div']
+>(function InnerBadge({ type = 'default', color, children }, ref) {
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        'inline-block rounded-xl px-2 py-0.5 text-xs whitespace-nowrap',
+        {
+          accent: 'bg-accent-500',
+          default: 'bg-primary-300',
+          good: 'bg-green-300',
+        }[type]
+      )}
+      style={{
+        ...(color && { backgroundColor: color }),
+      }}
+    >
+      {children}
+    </div>
+  );
+});
 
 export const Badge: React.FC<Props> = ({ children, hint, ...innerProps }) => {
   const inner = <InnerBadge {...innerProps}>{children}</InnerBadge>;
