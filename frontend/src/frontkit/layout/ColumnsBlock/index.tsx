77215032a220c290d0e55cfdecf71c37718c9296
Vyacheslav Matyukhin <me@berekuk.ru>
2021-06-21 14:32:58 +0300

    center columns on large screens

--- frontend/src/frontkit/layout/ColumnsBlock/index.tsx
+++ frontend/src/frontkit/layout/ColumnsBlock/index.tsx
@@ -1,6 +1,6 @@
 /* Note that this component doesn't include any padding. You'll probably have to wrap it in <ResponsivePadding> or your own padding component. */
 export const ColumnsBlock: React.FC = ({ children }) => (
-  <div className="grid gap-5 lg:gap-10 lg:grid-flow-col lg:auto-cols-fr xl:gap-20 max-w-screen-2xl">
+  <div className="grid gap-5 lg:gap-10 lg:grid-flow-col lg:auto-cols-fr xl:gap-20 mx-auto max-w-screen-2xl">
     {children}
   </div>
 );
